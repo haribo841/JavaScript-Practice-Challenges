a4c73e6a3719faf48c1aa0bc32339f6c89e413cc
haribo841 <adamwaldemarkubis@gmail.com>
2022-07-25 16:24:16 +0200

    Archive
    Minimal Dungeon RPG Demo

---
+++ cocosExport/web-desktop/src/settings.eb973.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Game.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"803f8",resources:"0ac40",main:"d9cc7"}};
