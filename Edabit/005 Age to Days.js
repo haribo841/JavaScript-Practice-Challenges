10204a2517f2199327daea30d06f07bc188cdf46
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-02-26 22:50:59 +0100

    Solved 5 Age to Days

---
+++ Edabit/005 Age to Days.js
@@ -0,0 +1,9 @@
+//Create a function that takes the age in years and returns the age in days.
+function calcAge(age) {
+    return age * 365;
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(calcAge(10), 3650)
+Test.assertEquals(calcAge(0), 0)
+Test.assertEquals(calcAge(73), 26645)
+console.log(calcAge());
