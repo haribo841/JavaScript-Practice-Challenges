4feebece47e2e201cf9d81a880c451f92adb940e
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-03-01 17:14:54 +0100

    Solved 7 Remainder from Two Numbers

---
+++ Edabit/007 Remainder from Two Numbers.js
@@ -0,0 +1,12 @@
+/*There is a single operator in C#, capable of providing the remainder of a division operation.
+Two numbers are passed as parameters. The first parameter divided by the second parameter will
+have a remainder, possibly zero. Return that value.*/
+function remainder(x, y) {
+    return x % y;
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(remainder(7, 2), 1)
+Test.assertEquals(remainder(3, 4), 3)
+Test.assertEquals(remainder(-9, 45), -9)
+Test.assertEquals(remainder(5, 5), 0)
+console.log(remainder());
