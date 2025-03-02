9e0fda9548fa55fa1594a98bebc5be476611b958
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-03-02 13:52:59 +0100

    Solved 8 Less than or Equal to Zero

---
+++ Edabit/008 Less than or Equal to Zero.js
@@ -0,0 +1,9 @@
+//Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+function lessThanOrEqualToZero(num) {
+    return num <= 0;
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(lessThanOrEqualToZero(5), false)
+Test.assertEquals(lessThanOrEqualToZero(0), true)
+Test.assertEquals(lessThanOrEqualToZero(-5), true)
+console.log(lessThanOrEqualToZero());
