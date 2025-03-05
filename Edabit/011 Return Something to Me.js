8837dfe9a9716ca7121b7e9d48a10feceaa4fc63
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-03-05 22:02:55 +0100

    Solved 11 Return Something to Me

---
+++ Edabit/011 Return Something to Me.js
@@ -0,0 +1,9 @@
+//Write a function that returns the string "something" joined with a space " " and the given argument a.
+function giveMeSomething(a) {
+    return `something ${a}`;
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(giveMeSomething("a"), "something a")
+Test.assertEquals(giveMeSomething("is cooking"), "something is cooking")
+Test.assertEquals(giveMeSomething(" is cooking"), "something  is cooking")
+console.log(giveMeSomething());
