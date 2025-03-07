5875520fb19c6f379d982c0ce5e8c83d735ef795
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-03-07 22:40:42 +0100

    Solved 13 Hours into Seconds

---
+++ Edabit/013 Hours into Seconds.js
@@ -0,0 +1,10 @@
+//Write a function that converts hours into seconds.
+function howManySeconds(hours) {
+    return hours * 3600;
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(howManySeconds(2), 7200)
+Test.assertEquals(howManySeconds(10), 36000)
+Test.assertEquals(howManySeconds(24), 86400)
+Test.assertEquals(howManySeconds(36), 129600)
+console.log(howManySeconds());
