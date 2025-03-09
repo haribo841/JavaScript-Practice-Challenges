6897436bd56afea57dde6a497ceffd73503005a4
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-03-09 16:24:01 +0100

    Solved 15 Are the Numbers Equal.

---
+++ Edabit/015 Are the Numbers Equal.js
@@ -0,0 +1,14 @@
+//Create a function that takes two integers and checks if they are equal.
+function isEqual(num1, num2) {
+    return num1 === num2;
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(isEqual(2, 2), true)
+Test.assertEquals(isEqual("1", 5), false)
+Test.assertEquals(isEqual(88, 88), true)
+Test.assertEquals(isEqual(36, 35), false)
+Test.assertEquals(isEqual("1", 1), false)
+Test.assertEquals(isEqual(1, 1), true)
+Test.assertEquals(isEqual(5, 6), false)
+Test.assertEquals(isEqual("1", "1"), false)
+console.log(isEqual());
