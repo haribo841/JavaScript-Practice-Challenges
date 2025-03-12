eb042335baa9149c670890c094f2aeca9b3550c1
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-03-12 23:30:11 +0100

    Solved 18 Basketball Points

---
+++ Edabit/018 Basketball Points.js
@@ -0,0 +1,14 @@
+//You are counting points for a basketball game,
+//given the amount of 3-pointers scored and 2-pointers scored,
+//find the final points for the team and return that value
+//([2 -pointers scored, 3-pointers scored]).
+function points(twoPointers, threePointers) {
+    return twoPointers * 2 + threePointers * 3;
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(points(1, 1), 5)
+Test.assertEquals(points(1, 2), 8)
+Test.assertEquals(points(2, 1), 7)
+Test.assertEquals(points(2, 2), 10)
+Test.assertEquals(points(69, 420), 1398)
+console.log(points());
