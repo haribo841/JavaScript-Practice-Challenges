858aaf817327aa671c91efe38c887dff4276c585
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-03-13 22:56:06 +0100

    Solved 19 Perimeter of a Rectangle

---
+++ Edabit/019 Perimeter of a Rectangle.js
@@ -0,0 +1,9 @@
+//Create a function that takes length and width and finds the perimeter of a rectangle.
+function findPerimeter(length, width) {
+    return length * 2 + width * 2;
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(findPerimeter(6, 7), 26)
+Test.assertEquals(findPerimeter(20, 10), 60)
+Test.assertEquals(findPerimeter(2, 9), 22)
+console.log(findPerimeter());
