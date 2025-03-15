054883901a9732d607700d430592a5b861a53571
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-03-15 08:01:18 +0100

    Solved 20 Name Greeting!

---
+++ Edabit/020 Name Greeting!.js
@@ -0,0 +1,9 @@
+//Create a function that takes a name and returns a greeting in the form of a string.
+//Don't use a normal function, use an arrow function.
+const helloName = name => `Hello ${name}!`;
+const Test = require('../testHelper.js');
+Test.assertEquals(helloName("Gerald"), "Hello Gerald!")
+Test.assertEquals(helloName("Fatima"), "Hello Fatima!")
+Test.assertEquals(helloName("Ed"), "Hello Ed!")
+Test.assertEquals(helloName("Tiffany"), "Hello Tiffany!")
+console.log(helloName());
