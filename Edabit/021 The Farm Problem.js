a84fc20ea74e1e23fb92e8b7bb2131541f657d34
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-03-16 19:04:05 +0100

    Solved 21 The Farm Problem

---
+++ Edabit/021 The Farm Problem.js
@@ -0,0 +1,16 @@
+//In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals.
+//The farmer breeds three species:
+//chickens = 2 legs
+//cows = 4 legs
+//pigs = 4 legs
+//The farmer has counted his animals and he gives you a subtotal for each species.
+//You have to implement a function that returns the total number of legs of all the animals.
+function animals(chickens, cows, pigs) {
+    return chickens * 2 + cows * 4 + pigs * 4;
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(animals(5, 2, 8), 50)
+Test.assertEquals(animals(3, 4, 7), 50)
+Test.assertEquals(animals(1, 2, 3), 22)
+Test.assertEquals(animals(3, 5, 2), 34)
+console.log(animals());
