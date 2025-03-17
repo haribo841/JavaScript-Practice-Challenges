988de77eec8e4a008b1d045cdd355d9b9d227140
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-03-17 23:51:17 +0100

    Solved 22 Football Points

---
+++ Edabit/022 Football Points.js
@@ -0,0 +1,12 @@
+//Create a function that takes the number of wins,
+//draws and losses and calculates the number of points a football team has obtained so far.
+function footballPoints(wins, draws, losses) {
+    return wins * 3 + draws;
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(footballPoints(1, 2, 3), 5)
+Test.assertEquals(footballPoints(5, 5, 5), 20)
+Test.assertEquals(footballPoints(1, 0, 0), 3)
+Test.assertEquals(footballPoints(0, 7, 0), 7)
+Test.assertEquals(footballPoints(0, 0, 15), 0)
+console.log(footballPoints());
