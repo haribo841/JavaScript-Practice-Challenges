cdf1099182285fdec777eab230fe6a4b2f1b4689
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-03-19 22:38:04 +0100

    Solved 23 First Element in an Array

---
+++ Edabit/023 First Element in an Array.js
@@ -0,0 +1,15 @@
+//Create a function that takes an array containing only values of the same type and return the first element.
+function getFirstValue(arr) {
+    if (!Array.isArray(arr)) {
+        return undefined; // or handle the error as needed
+    }
+    return arr[0];
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(getFirstValue([1, 2, 3]), 1)
+Test.assertEquals(getFirstValue([80, 5, 100]), 80)
+Test.assertEquals(getFirstValue([-500, 0, 50]), -500)
+Test.assertEquals(getFirstValue([5, 2, 3]), 5)
+Test.assertEquals(getFirstValue([75675, 5, 100]), 75675)
+Test.assertEquals(getFirstValue([-52320, 0, 50]), -52320)
+console.log(getFirstValue());
