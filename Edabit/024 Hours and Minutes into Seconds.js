1167032e5f6d25297cb1176a9f3a5f1759ccd09c
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-03-20 22:18:38 +0100

    Solved 24 Hours and Minutes into Seconds

---
+++ Edabit/024 Hours and Minutes into Seconds.js
@@ -0,0 +1,9 @@
+//Write a function that takes two integers (hours, minutes), converts them to seconds, and adds them.
+function convert(hours, minutes) {
+    return hours * 3600 + minutes * 60;
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(convert(1, 0), 3600)
+Test.assertEquals(convert(1, 3), 3780)
+Test.assertEquals(convert(0, 30), 1800)
+console.log(convert());
