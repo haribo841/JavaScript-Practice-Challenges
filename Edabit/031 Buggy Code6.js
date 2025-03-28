a9ed90609b8a8c5394f88bce15db31b143594d7f
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-03-28 21:48:03 +0100

    Solved 31 Buggy Code6

---
+++ Edabit/031 Buggy Code6.js
@@ -0,0 +1,11 @@
+//Mubashir wants to remove numbers from a given string!
+//Help him by fixing the code in the code tab to pass this challenge.
+function removeNumbers(str) {
+    return str.split('').filter(c => isNaN(parseInt(c))).join('');
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(removeNumbers("mubashir1"), "mubashir")
+Test.assertEquals(removeNumbers("12ma23tt"), "matt")
+Test.assertEquals(removeNumbers("e1d2a3b4i5t6"), "edabit")
+Test.assertEquals(removeNumbers("pakistan007"), "pakistan")
+Test.assertEquals(removeNumbers("ai4653rf53or4235ce"), "airforce")
