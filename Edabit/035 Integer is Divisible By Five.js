b9736e18af85fe337f313f30902b7f7587e36681
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-04-02 22:16:20 +0200

    Solved 35 Integer is Divisible By Five

---
+++ Edabit/035 Integer is Divisible By Five.js
@@ -0,0 +1,12 @@
+//Create a function that returns true if an integer is evenly divisible by 5, and false otherwise.
+function divisibleByFive(n) {
+    return n % 5 === 0;
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(divisibleByFive(7), false)
+Test.assertEquals(divisibleByFive(5), true)
+Test.assertEquals(divisibleByFive(15), true)
+Test.assertEquals(divisibleByFive(33), false)
+Test.assertEquals(divisibleByFive(-18), false)
+Test.assertEquals(divisibleByFive(999), false)
+Test.assertEquals(divisibleByFive(2), false)
