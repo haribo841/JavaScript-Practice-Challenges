5d646551d138b8db47f418d6bbabeba3d63dd6c0
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-04-03 19:16:00 +0200

    Solved 36 Correct the Mistakes

---
+++ Edabit/036 Correct the Mistakes.js
@@ -0,0 +1,10 @@
+//Fix the code in the code tab to pass this challenge (only syntax errors).
+function squared(b) {
+	return b * b
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(squared(10), 100, "Expected 100")
+Test.assertEquals(squared(69), 4761, "Expected 4761")
+Test.assertEquals(squared(666), 443556, "Expected 443556")
+Test.assertEquals(squared(-21), 441, "Expected 441")
+Test.assertEquals(squared(21), 441, "Expected 441")
