c1487d00bfb51e9ea517b415a0fde996a9e0db44
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-04-06 11:44:18 +0200

    Solved 39 Frames Per Second

---
+++ Edabit/039 Frames Per Second.js
@@ -0,0 +1,13 @@
+//Create a function that returns the number of frames shown in a given number of minutes for a certain FPS.
+function frames(minutes, fps) {
+    return minutes * fps * 60;
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(frames(1, 1), 60)
+Test.assertEquals(frames(10, 1), 600)
+Test.assertEquals(frames(10, 25), 15000)
+Test.assertEquals(frames(500, 60), 1800000)
+Test.assertEquals(frames(0, 60), 0)
+Test.assertEquals(frames(99, 1), 5940)
+Test.assertEquals(frames(419, 70), 1759800)
+Test.assertEquals(frames(52, 33), 102960)
