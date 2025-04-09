3380007a9a227662a7ecb66fd8ff6850ba30e3f0
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-04-09 02:06:15 +0200

    Solved 41 Multiple of 100

---
+++ Edabit/041 Multiple of 100.js
@@ -0,0 +1,12 @@
+//Create a function that takes an integer and returns true if it's divisible by 100, otherwise return false.
+function divisible(num) {
+    return num % 100 === 0;
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(divisible(1), false)
+Test.assertEquals(divisible(100), true)
+Test.assertEquals(divisible(1000), true)
+Test.assertEquals(divisible(111000), true)
+Test.assertEquals(divisible(-1), false, "Don't forget negatives.")
+Test.assertEquals(divisible(0), true, "Cover the 0 cases.")
+Test.assertEquals(divisible(-100), true, "-100 is divisible by 100.")
