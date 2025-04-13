55a7878b3a62c255ee473bf3dd93b564140b5c7b
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-04-13 20:09:02 +0200

    Solved 45 Check Whether a Given Number Is Odd

---
+++ Edabit/045 Check Whether a Given Number Is Odd.js
@@ -0,0 +1,13 @@
+//Write a function that takes a number n and returns the number of stacked boxes in a model n levels high, visible and invisible.
+function isOdd(num) {
+	return num % 2 != 0;
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(isOdd(-5), true)
+Test.assertEquals(isOdd(-50), false)
+Test.assertEquals(isOdd(0), false)
+Test.assertEquals(isOdd(25), true)
+Test.assertEquals(isOdd(-19), true)
+Test.assertEquals(isOdd(12), false)
+Test.assertEquals(isOdd(23), true)
+Test.assertEquals(isOdd(-71), true)
