2c53625effb15fce72a980cff1566e6f058a791f
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-04-17 23:45:33 +0200

    Solved 47 Free Coffee Cups

---
+++ Edabit/047 Free Coffee Cups.js
@@ -0,0 +1,11 @@
+//Create a function that takes a number n and returns the nth even number beginning with 0 as the first.
+function totalCups(n) {
+    return n + Math.floor(n/6);
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(totalCups(6), 7)
+Test.assertEquals(totalCups(3), 3)
+Test.assertEquals(totalCups(7), 8)
+Test.assertEquals(totalCups(12), 14)
+Test.assertEquals(totalCups(213), 248)
+Test.assertEquals(totalCups(16), 18)
