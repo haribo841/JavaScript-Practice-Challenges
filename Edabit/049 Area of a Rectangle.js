5d136a24e42358e1bbae7f61ec6ae1cc9ca3ddc2
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-04-19 20:10:13 +0200

    Solved 49 Area of a Rectangle

---
+++ Edabit/049 Area of a Rectangle.js
@@ -0,0 +1,15 @@
+//Create a function that calculates the area of a rectangle. If the arguments are invalid, your function must return -1.
+function area(h, w) {
+    return (h > 0 && w > 0) ? h * w : -1;
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(area(5, 3), 15)
+Test.assertEquals(area(8, 5), 40)
+Test.assertEquals(area(5, 4), 20)
+Test.assertEquals(area(2, 3), 6)
+Test.assertEquals(area(10000, 10000), 100000000)
+Test.assertEquals(area(-2, -5), -1)
+Test.assertEquals(area(0, 3), -1)
+Test.assertEquals(area(5, -3), -1)
+Test.assertEquals(area(0, 1), -1)
+Test.assertEquals(area(-1, 0), -1)
