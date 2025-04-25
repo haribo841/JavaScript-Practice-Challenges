2a4bfb3d90e3435c8e6cd45bdddaa4ceaeb5799b
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-04-25 23:31:46 +0200

    Solved 53 Return Negative

---
+++ Edabit/053 Return Negative.js
@@ -0,0 +1,14 @@
+//Create a function that takes a number as an argument and returns negative of that number.
+//Return negative numbers without any change.
+function returnNegative(n) {
+    return n < 0 ? n : -n;
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(returnNegative(4), -4)
+Test.assertEquals(returnNegative(15), -15)
+Test.assertEquals(returnNegative(-4), -4)
+Test.assertEquals(returnNegative(42), -42)
+Test.assertEquals(returnNegative(-9), -9)
+Test.assertEquals(returnNegative(0), 0)
+Test.assertEquals(returnNegative(1), -1)
+Test.assertEquals(returnNegative(-1), -1)
