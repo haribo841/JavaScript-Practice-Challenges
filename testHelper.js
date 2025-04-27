37a31c7c8428e6ca3a36e0a5b0947b27fc5e16cd
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-04-27 09:23:55 +0200

    Solved 55 Recreating Function & expanded Test helper +assertNotEquals

--- testHelper.js
+++ testHelper.js
@@ -1,12 +1,21 @@
 const Test = {
-    assertEquals: function (actual, expected, message) {
-        if (JSON.stringify(actual) === JSON.stringify(expected)) {
-            console.log("Test passed.");
-        } else {
-            console.error("Test failed:", message);
-            console.error("Expected:", expected, "but got:", actual);
-        }
-    },
+  assertEquals: function (actual, expected, message) {
+    if (JSON.stringify(actual) === JSON.stringify(expected)) {
+      console.log("Test passed.");
+    } else {
+      console.error("Test failed:", message);
+      console.error("Expected:", expected, "but got:", actual);
+    }
+  },
+
+  assertNotEquals: function (actual, notExpected, message) {
+    if (JSON.stringify(actual) !== JSON.stringify(notExpected)) {
+      console.log("Test passed.");
+    } else {
+      console.error("Test failed:", message);
+      console.error("Did not expect:", notExpected, "but got:", actual);
+    }
+  }
 };
 
 module.exports = Test;
