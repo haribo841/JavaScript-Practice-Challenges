50483e523b403d75347ff22a0683cce647f24841
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-05-02 15:39:27 +0200

    Solved 59 Length of a String

---
+++ Edabit/059 Length of a String.js
@@ -0,0 +1,11 @@
+//Write a function that returns the length of a string.
+//Make your function recursive.
+function length(str) {
+    return str === '' ? 0 : 1 + length(str.slice(1));
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(length('shipment'), 8)
+Test.assertEquals(length('apple'), 5)
+Test.assertEquals(length('make'), 4)
+Test.assertEquals(length('a'), 1)
+Test.assertEquals(length(''), 0)
