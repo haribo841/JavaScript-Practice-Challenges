ade0e3626e416ec4c5dba5f8f1d6305fcb6ce31e
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-05-06 19:46:11 +0200

    Solved 62 Check String for Spaces

---
+++ Edabit/062 Check String for Spaces.js
@@ -0,0 +1,12 @@
+//Create a function that returns true if a string contains any spaces.
+function hasSpaces(str) {
+    return str.includes(" ");
+}
+const Test = require('../testHelper.js');
+Test.assertEquals(hasSpaces("Foo"), false)
+Test.assertEquals(hasSpaces("Foo bar"), true)
+Test.assertEquals(hasSpaces("Foo "), true)
+Test.assertEquals(hasSpaces(" Foo"), true)
+Test.assertEquals(hasSpaces(" "), true)
+Test.assertEquals(hasSpaces(""), false)
+Test.assertEquals(hasSpaces(",./;'[]-="), false)
