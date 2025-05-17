1756232a5f918e4caf0ea8f77dd11293f6ad224a
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-05-17 10:46:22 +0200

    Solved 70 String to Integer and Vice Versa

---
+++ Edabit/070 String to Integer and Vice Versa.js
@@ -0,0 +1,19 @@
+//Write two functions:
+//toInt() : A function to convert a string to an integer.
+//toStr() : A function to convert an integer to a string.
+function toInt(str) {
+    return parseInt(str, 10);
+}
+
+function toStr(int) {
+    return int.toString();
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(toInt("37"), 37)
+assertEquals(toInt("113"), 113)
+assertEquals(toInt("5"), 5)
+assertEquals(toInt("5231"), 5231)
+assertEquals(toStr(37), "37")
+assertEquals(toStr(113), "113")
+assertEquals(toStr(5), "5")
+assertEquals(toStr(5231), "5231")
