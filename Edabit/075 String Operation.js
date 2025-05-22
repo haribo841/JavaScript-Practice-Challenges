7520f9effcd2cc87307b61a7ea10c01bf5009d4c
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-05-22 23:38:49 +0200

    Solved 75 String Operation

---
+++ Edabit/075 String Operation.js
@@ -0,0 +1,15 @@
+//Create a function that takes two numbers and a mathematical operator and returns the result.
+function calculate(num1, num2, op) {
+    return op === "+" ? num1 + num2 :
+           op === "-" ? num1 - num2 :
+           op === "*" ? num1 * num2 :
+           op === "/" ? num1 / num2 :
+           op === "%" ? num1 % num2 :
+           null;
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(calculate(24, 100, "-"), -76)
+assertEquals(calculate(-20, -30, "+"), -50)
+assertEquals(calculate(1500, 5, "/"), 300)
+assertEquals(calculate(38, 3, "*"), 114)
+assertEquals(calculate(49, 5, "%"), 4)
