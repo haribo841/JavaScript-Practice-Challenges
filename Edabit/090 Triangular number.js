a2615e646b32c3afdc3bff21029e2253b5623b12
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-08-28 22:17:57 +0200

    Solved 90 Triangular number

---
+++ Edabit/090 Triangular number.js
@@ -0,0 +1,21 @@
+//Create a function that takes a number as an argument.
+//Add up all the numbers from 1 to the number you passed to the function.
+function addUp(num) {
+    return num * (num + 1) / 2;
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(addUp(4), 10)
+assertEquals(addUp(13), 91)
+assertEquals(addUp(600), 180300)
+assertEquals(addUp(392), 77028)
+assertEquals(addUp(53), 1431)
+assertEquals(addUp(897), 402753)
+assertEquals(addUp(23), 276)
+assertEquals(addUp(1000), 500500)
+assertEquals(addUp(738), 272691)
+assertEquals(addUp(100), 5050)
+assertEquals(addUp(925), 428275)
+assertEquals(addUp(1), 1)
+assertEquals(addUp(999), 499500)
+assertEquals(addUp(175), 15400)
+assertEquals(addUp(111), 6216)
