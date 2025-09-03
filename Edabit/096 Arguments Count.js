60eeb87e7cc0d01db0d32ac7ca1218fa93e5c3f7
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-09-03 21:56:13 +0200

    Solved 96 Arguments Count

---
+++ Edabit/096 Arguments Count.js
@@ -0,0 +1,15 @@
+//Create a function that returns the number of arguments it was called with.
+function numArgs() {
+    return arguments.length;
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(numArgs(), 0)
+assertEquals(numArgs('foo'), 1)
+assertEquals(numArgs('foo', 'bar'), 2)
+assertEquals(numArgs(null, null), 2)
+assertEquals(numArgs('foo', 'bar', 5, null), 4)
+assertEquals(numArgs(false), 1)
+assertEquals(numArgs('foo', {}), 2)
+assertEquals(numArgs('foo', 'bar', {}), 3)
+assertEquals(numArgs([], [], {}), 3)
+assertEquals(numArgs('foo', 'bar', true, null, 0), 5)
