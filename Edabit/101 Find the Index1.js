31c040de8fbf3250cc667badcd81a2fe2e7b5f06
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-09-08 19:25:40 +0200

    Solved 101 Find the Index1

---
+++ Edabit/101 Find the Index1.js
@@ -0,0 +1,9 @@
+//Create a function that finds the index of a given item.
+function search(arr, item) {
+    return arr.indexOf(item);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(search([1, 5, 3], 5), 1)
+assertEquals(search([9, 8, 3], 3), 2)
+assertEquals(search([1, 2, 3], 4), -1)
+assertEquals(search([1, 5, 3, 10, 17, 20, -6], 20), 5)
