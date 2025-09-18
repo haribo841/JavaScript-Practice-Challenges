ab85971449a41beb19e57fe64182e3f9efec9f73
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-09-18 19:00:12 +0200

    Solved 079 Boolean to String Conversion Numeration change

--- Edabit/079 Find the Index.js
+++
@@ -1,8 +0,0 @@
-//Create a function that takes an array and a string as arguments and returns the index of the string.
-function findIndex(arr, str) {
-    return arr.indexOf(str);
-}
-import { assertEquals } from '../testHelper.js';
-assertEquals(findIndex(['abc', 'ghj', 'banana', 'grape'], 'grape'), 3)
-assertEquals(findIndex(['a', 'b', 'c', 'd', 'e', 'f'], 'f'), 5)
-assertEquals(findIndex(['hi', 'edabit.com', 'testcase', 'validstring'], 'edabit.com'), 1)
