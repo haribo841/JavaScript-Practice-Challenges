0697801413eea8969e77be44ff608473e56f5129
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-09-20 20:32:22 +0200

    Solved 111 Using Arrow Functions

---
+++ Edabit/111 Using Arrow Functions.js
@@ -0,0 +1,14 @@
+//Create a function that returns the given argument, but by using an arrow function.
+//An arrow function is constructed like so:
+//arrowFunc = (/*parameters*/) =>//code here
+function calculator(str) {
+    arrowFunc = (x) => x
+}
+import { assertEquals } from '../testHelper.js';
+if (!(String(arrowFunc).includes('=>'))) {
+    assertEquals(0, 1, "Your code does not use an arrow function")
+}
+assertEquals(arrowFunc(3), 3)
+assertEquals(arrowFunc("3"), "3")
+assertEquals(arrowFunc(true), true)
+assertEquals(arrowFunc("test"), "test")
