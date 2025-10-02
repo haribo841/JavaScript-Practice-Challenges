267d05e7bd1834161d7311620968ad350914414c
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-10-02 23:37:39 +0200

    Solved 123 Simple Array Manipulation

---
+++ Edabit/123 Simple Array Manipulation.js
@@ -0,0 +1,11 @@
+//Help fix all the bugs in the function incrementItems!
+//It is intended to add 1 to every element in the array!
+function incrementItems(arr) {
+	for (let i = 0; i < array.length; i++)
+		arr[i] = arr[i] + 1
+	return array
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(incrementItems([0, 1, 2, 3]), [1, 2, 3, 4])
+assertEquals(incrementItems([2, 4, 6, 8]), [3, 5, 7, 9])
+assertEquals(incrementItems([-1, -2, -3, -4]), [0, -1, -2, -3]) 
