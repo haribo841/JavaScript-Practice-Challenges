770e6ffec7b63edd0f40acddaad0daeb99a55374
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-10-03 12:22:38 +0200

    Solved 124 Century Crisis

---
+++ Edabit/124 Century Crisis.js
@@ -0,0 +1,13 @@
+/*Scientists have discovered that in four decades, the world will EXPLODE!
+It will also take three decades to make a spaceship to travel to a new planet that can hold the entire world population.
+You must calculate the number of people there will be in three decades from now.
+The variable population is the world population now.
+Assume that every month, someone gives birth to more people n.
+Return the number of people there will be when the spaceship is complete.*/
+function futurePeople(population, n) {
+    return population + n * 12 * 30;
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(futurePeople(256, 2), 976)
+assertEquals(futurePeople(3248, 6), 5408)
+assertEquals(futurePeople(5240, 3), 6320)
