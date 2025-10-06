d080b1320e6a9dd159ab48d5fa88e89387d6dfb9
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-10-06 13:35:36 +0200

    Solved 127 Using Ternary Operators

---
+++ Edabit/127 Using Ternary Operators.js
@@ -0,0 +1,7 @@
+﻿//Write a function that uses the ternary operator to return "yeah" if bool is true, and "nope" otherwise.
+function yeah_nope(bool) {
+    return bool ? "yeah" : "nope"
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(yeah_nope(true), "yeah")
+assertEquals(yeah_nope(false), "nope")
