cdef9e498eed6eaada6b10b22f62711deddd2374
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-10-12 13:41:10 +0200

    Solved 131 Minimal3

---
+++ Edabit/131 Minimal3.js
@@ -0,0 +1,13 @@
+﻿/*Write a function that returns the strings:
+"both" if both given booleans a and b are true.
+"first" if only a is true.
+"second" if only b is true .
+"neither" if both a and b are false.*/
+function areTrue(a, b) {
+    return a ? (b ? "both" : "first") : (b ? "second" : "neither");
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(areTrue(true, true), "both")
+assertEquals(areTrue(true, false), "first")
+assertEquals(areTrue(false, true), "second")
+assertEquals(areTrue(false, false), "neither")
