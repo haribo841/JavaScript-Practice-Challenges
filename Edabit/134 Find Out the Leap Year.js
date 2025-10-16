703ff462830ebba3f4e5f60de3ce83a67a8ee7b5
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-10-16 23:13:34 +0200

    Solved 134 Find Out the Leap Year

---
+++ Edabit/134 Find Out the Leap Year.js
@@ -0,0 +1,14 @@
+﻿//Write a function that determines if the year is a leap year or not.
+function leapYear(year) {
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(leapYear(2004), true)
+assertEquals(leapYear(8), true)
+assertEquals(leapYear(4), true)
+assertEquals(leapYear(2019), false)
+assertEquals(leapYear(1970), false)
+assertEquals(leapYear(2021), false)
+assertEquals(leapYear(1934), false)
+assertEquals(leapYear(1874), false)
+assertEquals(leapYear(1968), true)
