f577af55d12f6d26061a2c91a5ef0975ec608358
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-10-17 22:03:06 +0200

    Solved 135 Moving House

---
+++ Edabit/135 Moving House.js
@@ -0,0 +1,12 @@
+﻿//Given a person's age and the number of times they've moved house as moves,
+//return the average number of years that they've spent living in the same house.
+function yearsInOneHouse(age, moves) {
+    return Math.round(age / (moves + 1));
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(yearsInOneHouse(30, 1), 15)
+assertEquals(yearsInOneHouse(15, 2), 5)
+assertEquals(yearsInOneHouse(80, 0), 80)
+assertEquals(yearsInOneHouse(23, 2), 8)
+assertEquals(yearsInOneHouse(31, 2), 10)
+assertEquals(yearsInOneHouse(1, 0), 1)
