421bbf3bda7d1763c1b69ecd4f17699de42c7583
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-10-20 23:49:36 +0200

    Solved 136 Triangle and Parallelogram Area Finder

---
+++ Edabit/136 Triangle and Parallelogram Area Finder.js
@@ -0,0 +1,10 @@
+﻿//Write a function that accepts base (decimal), height (decimal) and shape ("triangle", "parallelogram") as input and calculates the area of that shape.
+function areaShape(base, height, shape) {
+    return shape === "triangle" ? +(base * height / 2).toFixed(3) : +(base * height).toFixed(3);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(areaShape(2, 3, "triangle"), 3)
+assertEquals(areaShape(8, 6, "parallelogram"), 48)
+assertEquals(areaShape(0, 1, "triangle"), 0)
+assertEquals(areaShape(2.9, 1.3, "parallelogram"), 3.77)
+assertEquals(areaShape(0.01, 5, "triangle"), 0.025)
