9dc7269501c39b6a9e77dc35473c441dcf9ce959
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-10-24 23:30:08 +0200

    Solved 140 Four Passengers and a Driver

---
+++ Edabit/140 Four Passengers and a Driver.js
@@ -0,0 +1,12 @@
+﻿//A typical car can hold four passengers and one driver, allowing five people to travel around.
+//Given n number of people, return how many cars are needed to seat everyone comfortably.
+function carsNeeded(n) {
+    return Math.ceil(n / 5);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(carsNeeded(0), 0)
+assertEquals(carsNeeded(1), 1)
+assertEquals(carsNeeded(4), 1)
+assertEquals(carsNeeded(5), 1)
+assertEquals(carsNeeded(6), 2)
+assertEquals(carsNeeded(18), 4)
