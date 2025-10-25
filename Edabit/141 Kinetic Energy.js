d5e38f158f9204e8377a5f71e9b44187db89f244
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-10-25 15:36:09 +0200

    Solved 141 Kinetic Energy

---
+++ Edabit/141 Kinetic Energy.js
@@ -0,0 +1,12 @@
+﻿//Return the Kinetic Energy in Joules, given the mass and velocity.
+//For the purposes of this challenge, round answers to the nearest integer.
+function kineticEnergy(m, v) {
+    return Math.round(0.5 * m * v * v);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(kineticEnergy(60, 3), 270)
+assertEquals(kineticEnergy(45, 10), 2250)
+assertEquals(kineticEnergy(63.5, 7.35), 1715)
+assertEquals(kineticEnergy(0, 7.35), 0)
+assertEquals(kineticEnergy(88.4, 0), 0)
+assertEquals(kineticEnergy(3.333, 5.4), 49)
