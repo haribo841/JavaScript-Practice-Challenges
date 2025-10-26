3bfe1c64e97c9db5f71ffdcad26fc98f5e4652e4
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-10-26 21:21:47 +0100

    Solved 142 Volume of a Box

---
+++ Edabit/142 Volume of a Box.js
@@ -0,0 +1,11 @@
+﻿//Create a function that takes an object argument sizes (contains width, length, height keys) and returns the volume of the box.
+function volumeOfBox(sizes) {
+    return sizes.width * sizes.length * sizes.height;
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(volumeOfBox({ width: 11, length: 1, height: 2 }), 22)
+assertEquals(volumeOfBox({ width: 1, length: 5, height: 3 }), 15)
+assertEquals(volumeOfBox({ width: 2, length: 1, height: 1 }), 2)
+assertEquals(volumeOfBox({ width: 2, length: 6, height: 5 }), 60)
+assertEquals(volumeOfBox({ width: 2, length: 5, height: 3 }), 30)
+assertEquals(volumeOfBox({ width: 2, length: 5, height: 1 }), 10)
