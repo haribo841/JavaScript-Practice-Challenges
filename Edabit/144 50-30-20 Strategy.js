3193329b9da6aceb6f97c94b1e0db02751cf79eb
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-10-28 23:48:36 +0100

    Solved 144 50-30-20 Strategy

---
+++ Edabit/144 50-30-20 Strategy.js
@@ -0,0 +1,21 @@
+﻿/*The 50-30-20 strategy is a simple way to budget,
+which involves spending 50% of after-tax income on needs,
+30% after tax income on wants,
+and 20% after-tax income on savings or paying off debt.
+
+Given the after-tax income as ati,
+what you are supposed to do is to make a function that will
+return an object that shows how much a person needs to spend on needs, wants, and savings.*/
+function fiftyThirtyTwenty(ati) {
+    return
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(fiftyThirtyTwenty(10000), { "Needs": 5000, "Wants": 3000, "Savings": 2000 })
+assertEquals(fiftyThirtyTwenty(50000), { "Needs": 25000, "Wants": 15000, "Savings": 10000 })
+assertEquals(fiftyThirtyTwenty(13450), { "Needs": 6725, "Wants": 4035, "Savings": 2690 })
+assertEquals(fiftyThirtyTwenty(27800), { "Needs": 13900, "Wants": 8340, "Savings": 5560 })
+assertEquals(fiftyThirtyTwenty(19000), { "Needs": 9500, "Wants": 5700, "Savings": 3800 })
+assertEquals(fiftyThirtyTwenty(17610), { "Needs": 8805, "Wants": 5283, "Savings": 3522 })
+assertEquals(fiftyThirtyTwenty(25000), { "Needs": 12500, "Wants": 7500, "Savings": 5000 })
+assertEquals(fiftyThirtyTwenty(100000), { "Needs": 50000, "Wants": 30000, "Savings": 20000 })
+assertEquals(fiftyThirtyTwenty(347100), { "Needs": 173550, "Wants": 104130, "Savings": 69420 })
