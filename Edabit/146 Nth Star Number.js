e7c13c4640a8c7b6d6b92ae7d6e484987bdf8966
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-11-01 23:55:58 +0100

    Solved 146 Nth Star Number

---
+++ Edabit/146 Nth Star Number.js
@@ -0,0 +1,14 @@
+﻿/*Create a function that takes a positive integer n and returns the nth "star number".
+A star number is a centered figurate number that represents a centered hexagram (six-pointed star),
+such as the one that Chinese checkers is played on.*/
+function starNumber(n) {
+    return 6 * n * (n - 1) + 1;
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(starNumber(2), 13)
+assertEquals(starNumber(3), 37)
+assertEquals(starNumber(6), 181)
+assertEquals(starNumber(6), 181)
+assertEquals(starNumber(8), 337)
+assertEquals(starNumber(6), 181)
+assertEquals(starNumber(11), 661)
