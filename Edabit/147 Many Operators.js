1e65fb41ba48e2d77046b0beb95acf03de92f2e7
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-11-02 19:56:57 +0100

    Solved 147 Many Operators

---
+++ Edabit/147 Many Operators.js
@@ -0,0 +1,16 @@
+﻿//Some basic arithmetic operators are +, -, *, /, and %.
+//In this challenge you will be given three parameters, num1, num2, and an operator.Use the operator on number 1 and 2.
+function operate(num1, num2, operator) {
+    return eval(`${num1} ${operator} ${num2}`);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(operate(1, 1, "+"), 2)
+assertEquals(operate(1, 1, "-"), 0)
+assertEquals(operate(1, 1, "*"), 1)
+assertEquals(operate(1, 1, "/"), 1)
+assertEquals(operate(1, 1, "%"), 0)
+assertEquals(operate(2, 1, "+"), 3)
+assertEquals(operate(2, 1, "-"), 1)
+assertEquals(operate(2, 1, "*"), 2)
+assertEquals(operate(2, 1, "/"), 2)
+assertEquals(operate(2, 1, "%"), 0)
