7e21fd2dd81b9ee3c09c253ddba1209cf324bbfc
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-11-07 23:09:07 +0100

    Solved 149 Luke, I Am Your

---
+++ Edabit/149 Luke, I Am Your.js
@@ -0,0 +1,23 @@
+﻿/*Luke Skywalker has family and friends.
+Help him remind them who is who.Given a string with a name, return the relation of that person to Luke.
+Test.assertEquals(relationToLuke("Darth Vader"), "Luke, I am your father.")
+Test.assertEquals(relationToLuke("Leia"), "Luke, I am your sister.")
+Test.assertEquals(relationToLuke("Han"), "Luke, I am your brother in law.")
+Test.assertEquals(relationToLuke("R2D2"), "Luke, I am your droid.")*/
+function relationToLuke(name) {
+     switch (name) {
+         case "Darth Vader":
+             return "Luke, I am your father.";
+         case "Leia":
+             return "Luke, I am your sister.";
+         case "Han":
+             return "Luke, I am your brother in law.";
+         default:
+             return "Luke, I am your droid.";
+    };
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(relationToLuke("Darth Vader"), "Luke, I am your father.")
+assertEquals(relationToLuke("Leia"), "Luke, I am your sister.")
+assertEquals(relationToLuke("Han"), "Luke, I am your brother in law.")
+assertEquals(relationToLuke("R2D2"), "Luke, I am your droid.")
