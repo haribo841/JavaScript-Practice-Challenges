d1547dc021db81773d0a893f29f5d9dce5331232
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-11-10 22:04:35 +0100

    Solved 152 Return Types

---
+++ Edabit/152 Return Types.js
@@ -0,0 +1,13 @@
+﻿//Create a function that takes an array and returns the types of values (data types) in a new array.
+function arrayValuesTypes(arr) {
+    return arr.map(item => typeof item);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(arrayValuesTypes([1, 10]), ['number', 'number']);
+assertEquals(arrayValuesTypes([["hello", 1], 10]), ['object', 'number']);
+assertEquals(arrayValuesTypes(["shashwat", 10, 90]), ['string', 'number', 'number']);
+assertEquals(arrayValuesTypes([[], true]), ['object', 'boolean']);
+assertEquals(arrayValuesTypes([false, false, "true"]), ['boolean', 'boolean', "string"]);
+assertEquals(arrayValuesTypes([1, 10]), ['number', 'number']);
+assertEquals(arrayValuesTypes([undefined, null]), ['undefined', 'object']);
+assertEquals(arrayValuesTypes([function () { let x = 10; }, 10]), ['function', 'number']);
