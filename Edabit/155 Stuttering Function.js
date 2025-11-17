b0ca837dbe6a4aa5f2f1766199fe33b2791465f3
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-11-17 17:07:37 +0100

    Solved 155 Stuttering Function

---
+++ Edabit/155 Stuttering Function.js
@@ -0,0 +1,10 @@
+﻿//Write a function that stutters a word as if someone is struggling to read it.
+//The first two letters are repeated twice with an ellipsis ... 
+//and space after each, and then the word is pronounced with a question mark?.
+function stutter(word) {
+    return `${word.slice(0, 2)}... ${word.slice(0, 2)}... ${word}?`;
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(stutter("incredible"), "in... in... incredible?")
+assertEquals(stutter("am"), "am... am... am?")
+assertEquals(stutter("outstanding"), "ou... ou... outstanding?")
