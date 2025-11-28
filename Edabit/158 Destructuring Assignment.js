521ab61699c58aca78f36e056df7525f73e2a8bd
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-11-28 23:10:16 +0100

    Solved 158 Destructuring Assignment

---
+++ Edabit/158 Destructuring Assignment.js
@@ -0,0 +1,22 @@
+﻿/*You can assign variables from arrays like this:
+arr = [1, 2, 3, 4, 5, 6, 7, 8]
+first = arr[0]
+second = arr[1]
+third = arr[2]
+other = arr.slice(3)
+
+console.log(first) ➞ outputs 1
+console.log(second) ➞ outputs 2
+console.log(third) ➞ outputs 3
+console.log(other) ➞ outputs [4, 5, 6, 7, 8]
+However, this is long and tedious.
+Instead, we can use Object Destructuring to create all those variables in one line.
+Your task: Create variables first, second, third and other from the given array using Destructuring Assignment
+Your task is to unpack the arr writeyourcodehere into four variables, first, second, third, and other.*/
+writeyourcodehere = [1, 2, 3, 4, 5, 6, 7, 8]
+const [first, second, third, ...other] = writeyourcodehere;
+import { assertEquals } from '../testHelper.js';
+assertEquals(first, 1)
+assertEquals(second, 2)
+assertEquals(third, 3)
+assertEquals(other, [4, 5, 6, 7, 8])
