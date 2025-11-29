5df4df403e6f87ee0f3c631708a023d9bc4180db
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-11-29 22:38:09 +0100

    Solved 159 Number Exist

---
+++ Edabit/159 Number Exist.js
@@ -0,0 +1,17 @@
+﻿//Create a function which validates whether a given number exists, and could represent a real life quantity.
+//Inputs will be given as a string.
+function validStrNumber(n) {
+    return /^(\d+|\d*\.\d+)$/.test(n);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(validStrNumber("3.2"), true)
+assertEquals(validStrNumber("324"), true)
+assertEquals(validStrNumber("54..4"), false)
+assertEquals(validStrNumber("number"), false)
+assertEquals(validStrNumber(".5"), true)
+assertEquals(validStrNumber("03"), true)
+assertEquals(validStrNumber("3.e"), false)
+assertEquals(validStrNumber("1234321"), true)
+assertEquals(validStrNumber(".42.3"), false)
+assertEquals(validStrNumber("0000000"), true)
+assertEquals(validStrNumber("000.000"), true)
