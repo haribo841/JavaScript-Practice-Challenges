fd35b3a61901e30a1b1a7d89443ad4bed03ff033
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-12-07 22:42:16 +0100

    Solved 161 Squares and Cubes

---
+++ Edabit/161 Squares and Cubes.js
@@ -0,0 +1,15 @@
+﻿//Create a function that takes an array of two numbers and checks if the square root of the first number is equal to the cube root of the second number.
+function checkSquareAndCube(arr) {
+    return Math.sqrt(arr[0]) === Math.cbrt(arr[1]);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(checkSquareAndCube([4, 8]), true)
+assertEquals(checkSquareAndCube([5, 12]), false)
+assertEquals(checkSquareAndCube([9, 27]), true)
+assertEquals(checkSquareAndCube([25, 120]), false)
+assertEquals(checkSquareAndCube([25, 125]), true)
+assertEquals(checkSquareAndCube([36, 215]), false)
+assertEquals(checkSquareAndCube([36, 217]), false)
+assertEquals(checkSquareAndCube([144, 1728]), true)
+assertEquals(checkSquareAndCube([1, 1]), true)
+assertEquals(checkSquareAndCube([676, 17576]), true)
