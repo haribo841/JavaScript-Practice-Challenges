0f42b5f6b4eee5a112eafe100c16ee2f8f52b36d
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2025-12-12 23:28:13 +0100

    Solved 164 Recreating Property

---
+++ Edabit/164 Recreating Property.js
@@ -0,0 +1,12 @@
+﻿//Create a function which returns the length of a string, WITHOUT using String.length property.
+function length(s) {
+    return s.split("").reduce((acc) => acc + 1, 0);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(length("Hello World"), 11)
+assertEquals(length("Edabit"), 6)
+assertEquals(length("wash your hands!"), 16)
+assertEquals(length("34445"), 5)
+assertEquals(length("   "), 3)
+assertEquals(length(""), 0)
+assertEquals(length("%%$"), 3)
