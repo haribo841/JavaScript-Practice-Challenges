20da4aebdf3bdfc1e5beebef6442c3eedb63eff3
Adam Kubiś <adamwaldemarkubis@gmail.com>
2025-12-24 22:27:46 +0100

    Solved 169 Multiply Every Array Item by Two

---
+++ Edabit/169 Multiply Every Array Item by Two.js
@@ -0,0 +1,8 @@
+﻿//Create a function that takes an array with numbers and return an array with the elements multiplied by two.
+function getMultipliedArr(arr) {
+    return arr.map(x => x * 2);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(getMultipliedArr([2, 5, 3]), [4, 10, 6])
+assertEquals(getMultipliedArr([1, 86, -5]), [2, 172, -10])
+assertEquals(getMultipliedArr([5, 382, 0])
