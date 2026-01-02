9ca759700aabce5208f51407d152fcab26690fc7
Adam Kubiś <adamwaldemarkubis@gmail.com>
2026-01-02 19:14:40 +0100

    Solved 172 Three Classes of Levers

---
+++ Edabit/172 Three Classes of Levers.js
@@ -0,0 +1,14 @@
+﻿/*Levers are simple machines with a rigid beam and a fulcrum.
+There are 3-types of levers: first class, second class and third class.
+In a first class lever, the fulcrum is situated in the middle with the effort and the load being positioned opposite of each other.
+In a second class lever, the fulcrum is situated in the right with the effort on the left and the load in the middle.
+In a third class lever, the fulcrum is situated in the left with the effort being in the middle and the load being on the right.
+Given an array that contains the fulcrum "f", the effort "e", and the load "l",
+write a function that determines whether or not the array shows a first class lever, second class lever, or a third class lever.*/
+function determineLever(arr) {
+    return arr.indexOf("f") === 1 ? "first class lever" : arr.indexOf("f") === 2 ? "second class lever" : "third class lever"
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(determineLever(["e", "f", "l"]), "first class lever")
+assertEquals(determineLever(["e", "l", "f"]), "second class lever")
+assertEquals(determineLever(["f", "e", "l"]), "third class lever")
