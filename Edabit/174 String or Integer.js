80a7d91a24e55d072bf841b2be7095f7d89a1901
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-01-05 23:11:03 +0100

    Solved 174 String or Integer

---
+++ Edabit/174 String or Integer.js
@@ -0,0 +1,11 @@
+﻿//Create a function that checks if the argument is an integer or a string.
+//Return "int" if it's an integer and "str" if it's a string.
+function intOrString(param) {
+    return typeof param === "number" ? "int" : "str";
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(intOrString(1), "int")
+assertEquals(intOrString("HELLO"), "str")
+assertEquals(intOrString("edabit"), "str")
+assertEquals(intOrString(873432874), "int")
+assertEquals(intOrString(5), "int")
