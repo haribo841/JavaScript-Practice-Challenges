d9f41391d563364ed8200a7c5adc1cae8ca544d1
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-01-27 23:24:40 +0100

    Solved 179 Burrrrrrrp

---
+++ Edabit/179 Burrrrrrrp.js
@@ -0,0 +1,12 @@
+﻿//Create a function that returns the string "Burp" with the amount of "r's" determined by the input parameters of the function.
+function longBurp(num) {
+    return "Bu" + "r".repeat(num) + "p";
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(longBurp(3), "Burrrp")
+assertEquals(longBurp(5), "Burrrrrp")
+assertEquals(longBurp(9), "Burrrrrrrrrp")
+assertEquals(longBurp(10), "Burrrrrrrrrrp")
+assertEquals(longBurp(13), "Burrrrrrrrrrrrrp")
+assertEquals(longBurp(18), "Burrrrrrrrrrrrrrrrrrp")
+assertEquals(longBurp(1), "Burp")
