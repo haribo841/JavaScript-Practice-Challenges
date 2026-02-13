63671b2d90afe4bf9bfa1b5de85077897dc0ff4c
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-02-13 19:22:47 +0100

    Solved 181 Perfect Roots

---
+++ Edabit/181 Perfect Roots.js
@@ -0,0 +1,17 @@
+﻿//Given a number n, find if its 2nd, 4th and 8th roots are all integers (perfect roots), return true if it exists, false if not.
+function perfectRoots(n) {
+    return Number.isInteger(Math.sqrt(n)) && Number.isInteger(Math.pow(n, 1 / 4)) && Number.isInteger(Math.pow(n, 1 / 8))
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(perfectRoots(256), true)
+assertEquals(perfectRoots(1000), false)
+assertEquals(perfectRoots(6561), true)
+assertEquals(perfectRoots(12534), false)
+assertEquals(perfectRoots(3455), false)
+assertEquals(perfectRoots(65536), true)
+assertEquals(perfectRoots(390625), true)
+assertEquals(perfectRoots(1679616), true)
+assertEquals(perfectRoots(6534561), false)
+assertEquals(perfectRoots(253456), false)
+assertEquals(perfectRoots(5764801), true)
+assertEquals(perfectRoots(100000000), true)
