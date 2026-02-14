bb19791837ce94702d52fe52d3e7dbffeaeeb5b3
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-02-14 20:52:10 +0100

    Solved 182 Cube the Square Root

---
+++ Edabit/182 Cube the Square Root.js
@@ -0,0 +1,8 @@
+﻿//Create a function that takes a number as an argument and returns the square root of that number cubed.
+function cubeSquareRoot(num) {
+    return Math.pow(Math.sqrt(num), 3)
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(cubeSquareRoot(81), 729)
+assertEquals(cubeSquareRoot(1646089), 2111932187)
+assertEquals(cubeSquareRoot(695556), 580093704)
