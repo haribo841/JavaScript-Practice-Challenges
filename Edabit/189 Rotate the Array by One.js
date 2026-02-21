4dd696e86fa41b0223f27f09e33ccea0c7dd122a
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-02-21 01:33:38 +0100

    Solved 189 Rotate the Array by One

---
+++ Edabit/189 Rotate the Array by One.js
@@ -0,0 +1,11 @@
+﻿//Given an array, rotate the values clockwise by one (the last value is sent to the first position).
+function rotateByOne(arr) {
+    return [arr[arr.length - 1], ...arr.slice(0, arr.length - 1)]
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(rotateByOne([1, 2, 3, 4, 5]), [5, 1, 2, 3, 4])
+assertEquals(rotateByOne([6, 5, 8, 9, 7]), [7, 6, 5, 8, 9])
+assertEquals(rotateByOne([20, 15, 26, 8, 4]), [4, 20, 15, 26, 8])
+assertEquals(rotateByOne([7, 8, 6, 4, 5]), [5, 7, 8, 6, 4])
+assertEquals(rotateByOne([5, 9, 45, 1, 2]), [2, 5, 9, 45, 1])
+assertEquals(rotateByOne([5, 9, 45, 1, 2, 8, 10, 12, 15]), [15, 5, 9, 45, 1, 2, 8, 10, 12])
