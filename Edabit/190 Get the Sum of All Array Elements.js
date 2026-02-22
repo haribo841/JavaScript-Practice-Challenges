2e08f3aa2610b11c3d65c2e2d67a185a23902756
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-02-22 01:52:59 +0100

    Solved 190 Get the Sum of All Array Elements

---
+++ Edabit/190 Get the Sum of All Array Elements.js
@@ -0,0 +1,8 @@
+﻿//Create a function that takes an array and returns the sum of all numbers in the array.
+function getSumOfItems(arr) {
+    return arr.reduce((acc, curr) => acc + curr, 0)
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(getSumOfItems([2, 7, 4]), 13)
+assertEquals(getSumOfItems([45, 3, 0]), 48)
+assertEquals(getSumOfItems([-2, 84, 23]), 105)
