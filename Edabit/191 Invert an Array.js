2a5fef3d084fb475a04f052e2f4b8eb62a3d82b8
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-02-23 17:38:10 +0100

    Solved 191 Invert an Array

---
+++ Edabit/191 Invert an Array.js
@@ -0,0 +1,9 @@
+﻿//Create a function that takes an array of numbers arr and returns an inverted array.
+function invertArray(arr) {
+    return arr.map((x) => x * -1);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(invertArray([1, 2, 3, 4, 5]), [-1, -2, -3, -4, -5])
+assertEquals(invertArray([1, -2, 3, -4, 5]), [-1, 2, -3, 4, -5])
+assertEquals(invertArray([]), [])
+assertEquals(invertArray([0]), [0])
