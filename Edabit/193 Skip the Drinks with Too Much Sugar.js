6baedb56cccd50fe4741a4753a01cdc5069f075f
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-02-25 19:22:25 +0100

    Solved 193 Skip the Drinks with Too Much Sugar

---
+++ Edabit/193 Skip the Drinks with Too Much Sugar.js
@@ -0,0 +1,15 @@
+﻿//Write a function that takes an array of drinks and returns an array of only drinks with no sugar in them.
+//Drinks that contain sugar(in this challenge) are:
+//cola
+//fanta
+function skipTooMuchSugarDrinks(drinks) {
+    return drinks.filter(drink => drink !== "cola" && drink !== "fanta");
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(skipTooMuchSugarDrinks(["cola", "fanta"]), [])
+assertEquals(skipTooMuchSugarDrinks(["cola", "fanta", "water"]), ["water"], "")
+assertEquals(skipTooMuchSugarDrinks(["cola"]), [], "")
+assertEquals(skipTooMuchSugarDrinks([]), [], "")
+assertEquals(skipTooMuchSugarDrinks(["lemonade", "beer", "fanta"]), ["lemonade", "beer"], "")
+assertEquals(skipTooMuchSugarDrinks(["water", "cola"]), ["water"], "")
+assertEquals(skipTooMuchSugarDrinks(["water", "tea"]), ["water", "tea"], "")
