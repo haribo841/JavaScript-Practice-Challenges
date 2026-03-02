8b4aba51cb01b6507acbfa6a5ea3857456ade0d1
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-03-02 11:20:43 +0100

    Solved 196 Chinese Zodiac

---
+++ Edabit/196 Chinese Zodiac.js
@@ -0,0 +1,13 @@
+﻿//In mathematics and digital electronics, a binary number is a number expressed in the base-2 numeral system or binary numeral system.
+//Given an array of ones and zeroes of a binary number, return the equivalent decimal value.
+function binaryToDecimal(arr) {
+    return arr.reduce((acc, val, index) => acc + val * Math.pow(2, arr.length - 1 - index), 0);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(binaryToDecimal([0, 0, 0, 1]), 1)
+assertEquals(binaryToDecimal([0, 0, 1, 0]), 2)
+assertEquals(binaryToDecimal([1, 1, 1, 1]), 15)
+assertEquals(binaryToDecimal([0, 1, 1, 0]), 6)
+assertEquals(binaryToDecimal([1, 0, 1, 0, 1, 1, 1]), 87)
+assertEquals(binaryToDecimal([1, 1, 1, 1, 1, 0, 1, 1, 0, 1]), 1005)
+assertEquals(binaryToDecimal([1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1]), 2013)
