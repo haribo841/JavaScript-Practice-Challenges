ad6572f441bc63187eb3c0b0a140c6d4b478b410
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-03-07 17:56:34 +0100

    Solved 197 The Forbidden Letter

---
+++ Edabit/197 The Forbidden Letter.js
@@ -0,0 +1,11 @@
+﻿//Given a letter and an array of words, return whether the letter does not appear in any of the words.
+function forbiddenLetter(char, arr) {
+    return arr.every(word => !word.includes(char));
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(forbiddenLetter('e', ['rinse', 'and', 'repeat']), false)
+assertEquals(forbiddenLetter('d', ['python', 'javascript', 'ruby', 'fortran']), true)
+assertEquals(forbiddenLetter('a', ['spoon', 'fork', 'knife']), true)
+assertEquals(forbiddenLetter('b', ['test', 'dot', 'assert', 'equals']), true)
+assertEquals(forbiddenLetter('i', ['rock', 'paper', 'scissors']), false)
+assertEquals(forbiddenLetter('t', []), true)
