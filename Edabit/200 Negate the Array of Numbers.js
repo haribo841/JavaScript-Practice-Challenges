46b04d16c5842942430a859063bd9c149a806386
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-03-15 09:54:16 +0100

    Solved 200 Negate the Array of Numbers

---
+++ Edabit/200 Negate the Array of Numbers.js
@@ -0,0 +1,10 @@
+﻿//Given an array of numbers, negate all elements contained within.
+//Negating a positive value - +n will return -n, because all + 's are removed.
+//Negating a negative value--n will return n, because the first - turns the second minus into a +.
+function negate(arr) {
+    return arr.map(num => -num);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(negate([1, 2, 3, 4]), [-1, -2, -3, -4])
+assertEquals(negate([-1, -2, -3, -4]), [1, 2, 3, 4])
+assertEquals(negate([]), [])
