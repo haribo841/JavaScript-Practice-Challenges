a9640f134e6f989f43149096ad3453b6bd7ad6b7
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-03-16 22:20:09 +0100

    Solved 201 Boolean to String Conversion

---
+++ Edabit/201 Boolean to String Conversion.js
@@ -0,0 +1,7 @@
+﻿//Create a function that takes a boolean variable flag and returns it as a string.
+function boolToString(flag) {
+    return flag.toString();
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(boolToString(true), "true")
+assertEquals(boolToString(false), "false")
