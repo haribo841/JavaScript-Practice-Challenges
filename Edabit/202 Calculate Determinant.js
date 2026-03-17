a0fe3357e26b94e06c1af790e02ef73b006cfcfd
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-03-17 19:13:53 +0100

    Solved 202 Calculate Determinant

---
+++ Edabit/202 Calculate Determinant.js
@@ -0,0 +1,25 @@
+﻿//Create a function to calculate the determinant of a 2 * 2 matrix.
+//The determinant of the following matrix is: ad - bc:
+//[[a, b], [c, d]]
+function calcDeterminant(matrix) {
+    return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(calcDeterminant([
+	[1, 2],
+	[3, 4]
+]), -2)
+
+assertEquals(calcDeterminant([
+	[5, 3],
+	[3, 1]
+]), -4)
+
+assertEquals(calcDeterminant([
+	[1, 1],
+	[1, 1]
+]), 0)
+assertEquals(calcDeterminant([
+	[1, 1],
+	[1, 0]
+]), -1)
