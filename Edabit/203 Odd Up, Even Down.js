6767e80834e004299afef127a902512534cefbdb
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-03-18 22:51:45 +0100

    Solved 203 Odd Up, Even Down

---
+++ Edabit/203 Odd Up, Even Down.js
@@ -0,0 +1,9 @@
+﻿//Create a function that goes through the array, incrementing (+1) for each odd-valued number and decrementing (-1) for each even-valued number.
+function transform(arr) {
+    return arr.map(x => x % 2 === 0 ? x - 1 : x + 1);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(transform([1, 2, 3, 4, 5]), [2, 1, 4, 3, 6])
+assertEquals(transform([3, 3, 4, 3]), [4, 4, 3, 4])
+assertEquals(transform([2, 2, 0, 8, 10]), [1, 1, -1, 7, 9])
+assertEquals(transform([9, 13, 15, 5, 2, 11]), [10, 14, 16, 6, 1, 12])
