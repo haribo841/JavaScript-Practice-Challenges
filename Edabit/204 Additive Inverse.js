e40beff34dace61eae9b9f5aaa474e171f5e86cb
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-03-19 22:03:23 +0100

    Solved 204 Additive Inverse

---
+++ Edabit/204 Additive Inverse.js
@@ -0,0 +1,8 @@
+﻿//A number added with its additive inverse equals zero. Create a function that returns an array of additive inverses.
+function additiveInverse(arr) {
+    return arr.map(x => -x);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(additiveInverse([5, -7, 8, 3]), [-5, 7, -8, -3])
+assertEquals(additiveInverse([1, 1, 1, 1, 1]), [-1, -1, -1, -1, -1])
+assertEquals(additiveInverse([-5, -25, 35]), [5, 25, -35])
