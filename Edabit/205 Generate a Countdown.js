3c2d36e3dcacefea33d6fc570d7dfe66a9cc13e0
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-03-21 19:07:01 +0100

    Solved 205 Generate a Countdown

---
+++ Edabit/205 Generate a Countdown.js
@@ -0,0 +1,9 @@
+﻿//Create a function that takes a number as an argument and returns an array of numbers counting down from this number to zero.
+function countdown(start) {
+    return Array.from({ length: start + 1 }, (_, i) => start - i);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(countdown(3), [3, 2, 1, 0])
+assertEquals(countdown(20), [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
+assertEquals(countdown(1), [1, 0])
+assertEquals(countdown(0), [0])
