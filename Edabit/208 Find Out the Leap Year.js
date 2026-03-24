62f411f935a2d39fceb3b4db8fe22c2eda10e9df
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-03-24 22:24:25 +0100

    Solved 208 Find Out the Leap Year

---
+++ Edabit/208 Find Out the Leap Year.js
@@ -0,0 +1,16 @@
+﻿//A leap year happens every four years, so it's a year that is perfectly divisible by four.
+//However, if the year is a multiple of 100(1800, 1900, etc), the year must be divisible by 400.
+//Write a function that determines if the year is a leap year or not.
+function leapYear(year) {
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(leapYear(2004), true)
+assertEquals(leapYear(8), true)
+assertEquals(leapYear(4), true)
+assertEquals(leapYear(2019), false)
+assertEquals(leapYear(1970), false)
+assertEquals(leapYear(2021), false)
+assertEquals(leapYear(1934), false)
+assertEquals(leapYear(1874), false)
+assertEquals(leapYear(1968), true)
