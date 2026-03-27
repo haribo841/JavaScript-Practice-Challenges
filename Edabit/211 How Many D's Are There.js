be8c27953b0fff0fa006d688a0a5c65a266984e3
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-03-27 23:22:30 +0100

    Solved 211 How Many D's Are There

---
+++ Edabit/211 How Many D's Are There.js
@@ -0,0 +1,8 @@
+﻿//Create a function that counts how many D's are in a sentence.
+function countDs(sentence) {
+    return sentence.split('').filter(char => char.toLowerCase() === 'd').length;
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(countDs("My friend Dylan got distracted at school"), 4)
+assertEquals(countDs("Debris was scattered all over the place."), 2)
+assertEquals(countDs("The rodents hibernated in their den."), 3)
