6e66bcf0da4b79fdbf262d18c3355741bdf15b81
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-03-30 23:14:25 +0200

    Solved 214 Check Factors

---
+++ Edabit/214 Check Factors.js
@@ -0,0 +1,12 @@
+﻿//Write a function that returns true if all integers in an array are factors of a number, and false otherwise.
+function checkFactors(factors, num) {
+    return factors.every(factor => num % factor === 0);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(checkFactors([2, 3, 4], 12), true)
+assertEquals(checkFactors([1, 2, 3, 8], 12), false, '8 is not a factor of 12')
+assertEquals(checkFactors([1, 2, 50], 100), true)
+assertEquals(checkFactors([1, 9, 81], 81), true)
+assertEquals(checkFactors([5, 10, 50], 500), true)
+assertEquals(checkFactors([5, 10, 499], 500), false, '499 is not a factor of 500')
+assertEquals(checkFactors([3, 6], 9), false, '6 is not a factor of 9')
