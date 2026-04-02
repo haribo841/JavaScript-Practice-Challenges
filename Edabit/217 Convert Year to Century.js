458dff1c48e609fd3f3c6e1f72ac8c59722fe023
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-04-02 23:56:36 +0200

    Solved 217 Convert Year to Century

---
+++ Edabit/217 Convert Year to Century.js
@@ -0,0 +1,10 @@
+﻿//Write a function that takes a year and returns its corresponding century.
+function centuryFromYear(year) {
+    return Math.ceil(year / 100);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(centuryFromYear(2020), 21)
+assertEquals(centuryFromYear(200), 2)
+assertEquals(centuryFromYear(2005), 21)
+assertEquals(centuryFromYear(1700), 17)
+assertEquals(centuryFromYear(1705), 18)
