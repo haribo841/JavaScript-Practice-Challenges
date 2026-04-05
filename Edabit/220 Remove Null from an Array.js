827a126d2acd535dc352dcff03999351d3cf13e9
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-04-05 11:08:47 +0200

    Solved 220 Remove Null from an Array

---
+++ Edabit/220 Remove Null from an Array.js
@@ -0,0 +1,9 @@
+﻿//Create a function to remove all null values from an array.
+function removeNull(arr) {
+    return arr.filter(item => item !== null);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(removeNull(['a', null, 'b', null]), ['a', 'b'])
+assertEquals(removeNull([null, null, null, null, null]), [])
+assertEquals(removeNull([7, 8, null, 9]), [7, 8, 9])
+assertEquals(removeNull([7, null, 8, null, 9]), [7, 8, 9])
