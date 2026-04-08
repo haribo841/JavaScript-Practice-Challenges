4ffae85ed0d1e91381f2669723efdcf5494ab888
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-04-08 20:01:57 +0200

    Solved 223 Mutating Arrays

---
+++ Edabit/223 Mutating Arrays.js
@@ -0,0 +1,12 @@
+﻿//Suppose I want to define a function that removes the last element of an array each time I call it, but does not mutate the original array.
+//Fix the code so that the results are no longer mutating the array.
+function minusOne(arr) {
+    return arr.slice(0, -1);
+}
+import { assertEquals } from '../testHelper.js';
+const x = [1, 2, 3, 4, 5]
+assertEquals(minusOne(x), [1, 2, 3, 4])
+assertEquals(minusOne(x), [1, 2, 3, 4])
+assertEquals(minusOne(x), [1, 2, 3, 4])
+assertEquals(minusOne(x), [1, 2, 3, 4])
+assertEquals(minusOne(x), [1, 2, 3, 4])
