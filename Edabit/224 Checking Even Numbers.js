d7f151cc208790b5400864a79c079275ff58752c
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-04-09 22:10:23 +0200

    Solved 224 Checking Even Numbers

---
+++ Edabit/224 Checking Even Numbers.js
@@ -0,0 +1,10 @@
+﻿//Create a function that takes in an array and returns true if all its values are even, and false otherwise.
+function checkAllEven(arr) {
+    return arr.every(x => x % 2 === 0);
+}
+import { assertEquals } from '../testHelper.js';
+const x = [1, 2, 3, 4, 5]
+assertEquals(checkAllEven([1, 2, 3, 4]), false)
+assertEquals(checkAllEven([2, 4, 6]), true)
+assertEquals(checkAllEven([5, 6, 8, 10]), false)
+assertEquals(checkAllEven([-2, 2, -2, 2]), true)
