3c768a31c0b699854c3e0fca35c8c70c58d3accd
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-04-10 23:40:55 +0200

    Solved 225 Product of All Odd Integers

---
+++ Edabit/225 Product of All Odd Integers.js
@@ -0,0 +1,10 @@
+﻿//Create a function that returns the product of all odd integers in an array.
+function oddProduct(arr) {
+    return arr.filter(x => x % 2 !== 0).reduce((product, x) => product * x, 1);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(oddProduct([3, 4, 1, 1, 5]), 15)
+assertEquals(oddProduct([5, 5, 8, 2, 4, 32]), 25)
+assertEquals(oddProduct([1, 2, 1, 2, 1, 2, 1, 2]), 1)
+assertEquals(oddProduct([0, 0, 0, 1]), 1)
+assertEquals(oddProduct([1, 2, 2, 5, 2, 0]), 5)
