f1cea4c2bf78bdeb1ba585a72419bcf7daa30d1f
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-04-11 17:30:32 +0200

    Solved 226 Matchstick Houses

---
+++ Edabit/226 Matchstick Houses.js
@@ -0,0 +1,13 @@
+﻿//Create a function that takes a number (step) as an argument and returns the number of matchsticks in that step.
+function matchHouses(step) {
+    return step === 0 ? 0 : 6 + (step - 1) * 5;
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(matchHouses(1), 6)
+assertEquals(matchHouses(0), 0)
+assertEquals(matchHouses(17), 86)
+assertEquals(matchHouses(36), 181)
+assertEquals(matchHouses(15), 76)
+assertEquals(matchHouses(99), 496)
+assertEquals(matchHouses(3), 16)
+assertEquals(matchHouses(87), 436)
