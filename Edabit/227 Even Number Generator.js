c562e4d1b339a760522ff82c9dbf6bb29de8664e
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-04-12 13:33:20 +0200

    Solved 227 Even Number Generator

---
+++ Edabit/227 Even Number Generator.js
@@ -0,0 +1,11 @@
+﻿//Create a function that finds all even numbers from 1 to the given number.
+function findEvenNums(num) {
+    return Array.from({ length: Math.floor(num / 2) }, (_, i) => (i + 1) * 2);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(findEvenNums(4), [2, 4])
+assertEquals(findEvenNums(8), [2, 4, 6, 8])
+assertEquals(findEvenNums(2), [2])
+assertEquals(findEvenNums(1), [])
+assertEquals(findEvenNums(9), [2, 4, 6, 8])
+assertEquals(findEvenNums(11), [2, 4, 6, 8, 10])
