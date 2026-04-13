8ab6e5de009d323a3ad7ad149c2f22c14ed7ecfc
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-04-13 21:24:35 +0200

    Solved 228 Same Number of Unique Elements

---
+++ Edabit/228 Same Number of Unique Elements.js
@@ -0,0 +1,12 @@
+﻿//Write a function that returns true if two arrays have the same number of unique elements, and false otherwise.
+function same(a1, a2) {
+    return new Set(a1).size === new Set(a2).size;
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(same([1, 3, 4, 4, 4], [2, 5, 7]), true)
+assertEquals(same([9, 8, 7, 6], [4, 4, 3, 1]), false)
+assertEquals(same([2], [3, 3, 3, 3, 3]), true)
+assertEquals(same([5, 6, 7, 9], [4, 1]), false)
+assertEquals(same([5, 9, 9], [9, 5]), true)
+assertEquals(same([1, 1, 1, 4], [1, 4, 4, 4, 4, 4]), true)
+assertEquals(same([], []), true)
