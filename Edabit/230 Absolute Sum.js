bb6bb0ae9eef4802116bfad17452d6062ccaeedb
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-04-15 23:39:08 +0200

    Solved 230 Absolute Sum

---
+++ Edabit/230 Absolute Sum.js
@@ -0,0 +1,10 @@
+﻿//Take an array of integers (positive or negative or both) and return the sum of the absolute value of each element.
+function getAbsSum(arr) {
+    return arr.reduce((acc, val) => acc + Math.abs(val), 0);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(getAbsSum([2, -1, -3, 4, 8]), 18);
+assertEquals(getAbsSum([-1]), 1);
+assertEquals(getAbsSum([-1, -3, -5, -4, -10, 0]), 23);
+assertEquals(getAbsSum([8, 9, 10, 32, 101, -10]), 170);
+assertEquals(getAbsSum([500]), 500);
