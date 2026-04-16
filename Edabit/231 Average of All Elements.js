e1f7876e7cb357f2f4356779d16fe83d2c391d18
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-04-16 21:52:32 +0200

    Solved 231 Average of All Elements

---
+++ Edabit/231 Average of All Elements.js
@@ -0,0 +1,11 @@
+﻿//Create a function that takes an array as an argument and returns true or false depending on whether the average of all elements in the array is a whole number or not.
+function isAvgWhole(arr) {
+    return arr.reduce((acc, val) => acc + val, 0) % arr.length === 0;
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(isAvgWhole([3, 5, 9]), false);
+assertEquals(isAvgWhole([1, 1, 1, 1]), true);
+assertEquals(isAvgWhole([1, 2, 3, 4, 5]), true);
+assertEquals(isAvgWhole([5, 2, 4]), false);
+assertEquals(isAvgWhole([11, 22]), false);
+assertEquals(isAvgWhole([4, 1, 7, 9, 2, 5, 7, 2, 4]), false);
