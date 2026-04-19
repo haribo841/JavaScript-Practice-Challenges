6f8639948adfb7cad54843d6bf5177994dbc6537
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-04-19 12:49:21 +0200

    Solved 234 Date Format

---
+++ Edabit/234 Date Format.js
@@ -0,0 +1,8 @@
+﻿//Create a function that converts a date formatted as MM/DD/YYYY to YYYYDDMM.
+function formatDate(date) {
+    return date.split('/').reverse().join('');
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(formatDate("11/12/2019"), "20191211")
+assertEquals(formatDate("12/31/2019"), "20193112")
+assertEquals(formatDate("01/15/2019"), "20191501")
