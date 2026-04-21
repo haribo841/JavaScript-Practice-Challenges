0d6f650a8ff8c5bc714be25804dca7a31544c5c9
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-04-21 21:50:57 +0200

    Solved 236 Return the Factorial

---
+++ Edabit/236 Return the Factorial.js
@@ -0,0 +1,11 @@
+﻿//Create a function that takes an integer and returns the factorial of that integer.
+//That is, the integer multiplied by all positive lower integers.
+function factorial(int) {
+    return int === 0 ? 1 : int * factorial(int - 1);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(factorial(2), 2)
+assertEquals(factorial(6), 720)
+assertEquals(factorial(3), 6)
+assertEquals(factorial(12), 479001600)
+assertEquals(factorial(5), 120)
