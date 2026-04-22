f0cd5fb44ee2210c4d3932d163b76b5a92da97b8
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-04-22 18:08:22 +0200

    Solved 237 Fix the Broken Code

---
+++ Edabit/237 Fix the Broken Code.js
@@ -0,0 +1,16 @@
+﻿//A student learning JavaScript was trying to make a function that sorts all the letters of a word,
+//but their code is broken, and they can't figure out why.
+//Spot and fix the error(s) in the code to make the function work.
+function sortWord(word) {
+	return word.split('').sort().join('');
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(sortWord("dcba"), "abcd")
+assertEquals(sortWord("pneumonoultramicroscopicsilicovolcanoconiosis"), "aacccccceiiiiiilllmmnnnnooooooooopprrsssstuuv")
+assertEquals(sortWord("Unpredictable"), "Uabcdeeilnprt", "Capital letters should come first!")
+assertEquals(sortWord("a"), "a")
+assertEquals(sortWord("ability"), "abiilty")
+assertEquals(sortWord("able"), "abel")
+assertEquals(sortWord("about"), "abotu")
+assertEquals(sortWord("above"), "abeov")
+assertEquals(sortWord("accept"), "accept")
