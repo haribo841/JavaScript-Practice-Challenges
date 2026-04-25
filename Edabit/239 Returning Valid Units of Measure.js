d5ec620c71f4c72275c86b468cabd7e2484cbd20
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-04-25 12:42:39 +0200

    Solved 239 Returning Valid Units of Measure

---
+++ Edabit/239 Returning Valid Units of Measure.js
@@ -0,0 +1,17 @@
+﻿//There has been a masterdata issue which affected the unit of measure of the products.
+//All values need to be checked if they are valid.
+//The unit of measure is valid when it is either "L"(liters), "PCE"(pieces) OR when the product has a comment.
+//The return value should be a Boolean.
+function hasValidUnitOfMeasure(product = {}) {
+	const { unitOfMeasure, comment } = product
+	return (Boolean(comment) || unitOfMeasure === 'L' || unitOfMeasure === 'PCE')
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(hasValidUnitOfMeasure({ "product": "Milk", unitOfMeasure: "L" }), true)
+assertEquals(hasValidUnitOfMeasure({ "product": "Cheese", unitOfMeasure: "PCE" }), true)
+assertEquals(hasValidUnitOfMeasure({ "product": "Eggs", comment: "Eggs are too different in size" }), true)
+//assertEquals(hasValidUnitOfMeasure({ "product": "Cerials", unitOfMeasure: "" }), false)
+assertEquals(hasValidUnitOfMeasure({ "product": "Flour" }), false)
+assertEquals(hasValidUnitOfMeasure({ "product": "Beer", unitOfMeasure: false }), false)
+assertEquals(hasValidUnitOfMeasure({ "product": "Beef", unitOfMeasure: "Cow" }), false)
+assertEquals(hasValidUnitOfMeasure(), false)
