17f73b5eb2e30b97261e6db8d020118c9c2f6322
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-04-28 20:42:27 +0200

    Solved 242 Taxi Journey

---
+++ Edabit/242 Taxi Journey.js
@@ -0,0 +1,13 @@
+﻿//A taxi journey costs $3 for the first kilometer travelled. However, all kilometers travelled after that will cost $2 each.
+//Create a function which returns the distance that the taxi must've travelled, given the cost as a parameter.
+function journeyDistance(num) {
+    return (num < 3 ? 0 : (Math.ceil((num - 3) / 2)) + 1);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(journeyDistance(0), 0)
+assertEquals(journeyDistance(3), 1)
+assertEquals(journeyDistance(5), 2)
+assertEquals(journeyDistance(9), 4)
+assertEquals(journeyDistance(15), 7)
+assertEquals(journeyDistance(99), 49)
+assertEquals(journeyDistance(11037), 5518)
