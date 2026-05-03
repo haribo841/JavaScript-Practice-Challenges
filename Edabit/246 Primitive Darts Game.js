1628cad0fbbd9d49eba05cfa8f1e7d5a581dd51f
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-05-03 11:40:15 +0200

    Solved 246 Primitive Darts Game

---
+++ Edabit/246 Primitive Darts Game.js
@@ -0,0 +1,19 @@
+﻿//Convert cartesian coordinates (x, y) to polar coordinates (R, phi) and return score based on the R value.
+//R > 10 gives 0 points, 10 >= R > 5 gives 1 point, 5 >= R > 1 gives 5 points, R <= 1 gives 10 points.
+function dartsScoring(x, y) {
+    return Math.sqrt(x ** 2 + y ** 2) > 10 ? 0 : Math.sqrt(x ** 2 + y ** 2) > 5 ? 1 : Math.sqrt(x ** 2 + y ** 2) > 1 ? 5 : 10;
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(dartsScoring(-9, 9), 0)
+assertEquals(dartsScoring(0, 10), 1)
+assertEquals(dartsScoring(-5, 0), 5)
+assertEquals(dartsScoring(0, -1), 10)
+assertEquals(dartsScoring(0, 0), 10)
+assertEquals(dartsScoring(-0.1, -0.1), 10)
+assertEquals(dartsScoring(0.7, 0.7), 10)
+assertEquals(dartsScoring(0.8, -0.8), 5)
+assertEquals(dartsScoring(-3.5, 3.5), 5)
+assertEquals(dartsScoring(-3.6, -3.6), 1)
+assertEquals(dartsScoring(-7.0, 7.0), 1)
+assertEquals(dartsScoring(7.1, -7.1), 0)
+assertEquals(dartsScoring(0.5, -4), 5)
