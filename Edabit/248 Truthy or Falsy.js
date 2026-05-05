07cdc533fa229c29dc5dfe0a272b9265e1a745f4
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-05-05 22:22:42 +0200

    Solved 248 Truthy or Falsy

---
+++ Edabit/248 Truthy or Falsy.js
@@ -0,0 +1,19 @@
+﻿//Create a function that takes an argument of any data type and returns 1 if it's truthy and 0 if it's falsy.
+function isTruthy(input) {
+    return input ? 1 : 0;
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(isTruthy(1), 1)
+assertEquals(isTruthy(-1), 1)
+assertEquals(isTruthy("false"), 1)
+assertEquals(isTruthy([]), 1)
+assertEquals(isTruthy(true), 1)
+assertEquals(isTruthy({}), 1)
+assertEquals(isTruthy(function () { }), 1)
+assertEquals(isTruthy(0), 0)
+assertEquals(isTruthy(""), 0)
+assertEquals(isTruthy(null), 0)
+assertEquals(isTruthy(undefined), 0)
+assertEquals(isTruthy(false), 0)
+assertEquals(isTruthy(NaN), 0)
+assertEquals(isTruthy(''), 0)
