3cc144a0ebadfd21aa686038fbe9835a1aa36057
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-05-06 11:42:56 +0200

    Solved 249 After N Months

---
+++ Edabit/249 After N Months.js
@@ -0,0 +1,11 @@
+﻿//Create a function that takes in year and months as input, then return what year it would be after n-months have elapsed.
+function afterNMonths(year, months) {
+    return months === undefined ? "month missing" : year === undefined ? "year missing" : year + Math.floor(months / 12);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(afterNMonths(2020, 24), 2022)
+assertEquals(afterNMonths(1832, 2), 1832)
+assertEquals(afterNMonths(1444, 60), 1449)
+assertEquals(afterNMonths(2020, undefined), "month missing")
+assertEquals(afterNMonths(undefined, 2), "year missing")
+assertEquals(afterNMonths(1444, 65), 1449)
