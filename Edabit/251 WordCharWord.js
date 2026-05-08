006ed09fbe74aafbd6da7c26eb43cc7f42c6b21d
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-05-08 21:42:58 +0200

    Solved 251 WordCharWord

---
+++ Edabit/251 WordCharWord.js
@@ -0,0 +1,9 @@
+﻿//Create a function that will put the first argument, a character, between every word in the second argument, a string.
+function add(char, str) {
+        return str.split(" ").join(char);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(add("#", "hello world"), "hello#world")
+assertEquals(add("R", "javascript is fun"), "javascriptRisRfun")
+assertEquals(add("*", "use .join() for this challenge"), "use*.join()*for*this*challenge")
+assertEquals(add("#", " "), "#")
