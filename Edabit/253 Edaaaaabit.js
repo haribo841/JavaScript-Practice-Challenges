a8b3c70da680760584e22a381dcee4699650f6fb
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-05-10 12:13:58 +0200

    Solved 253 Edaaaaabit

---
+++ Edabit/253 Edaaaaabit.js
@@ -0,0 +1,10 @@
+﻿//Write a function that takes an integer and returns a string with the given number of "a"s in Edabit.
+function howManyTimes(num) {
+    return "Ed" + "a".repeat(num) + "bit"
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(howManyTimes(5), "Edaaaaabit")
+assertEquals(howManyTimes(15), "Edaaaaaaaaaaaaaaabit")
+assertEquals(howManyTimes(10), "Edaaaaaaaaaabit")
+assertEquals(howManyTimes(23), "Edaaaaaaaaaaaaaaaaaaaaaaabit")
+assertEquals(howManyTimes(1), "Edabit")
