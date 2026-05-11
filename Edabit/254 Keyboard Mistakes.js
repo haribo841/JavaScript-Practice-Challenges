40efce02695d89b5a8eaddcc006424894ccc0d06
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-05-11 19:08:09 +0200

    Solved 254 Keyboard Mistakes

---
+++ Edabit/254 Keyboard Mistakes.js
@@ -0,0 +1,15 @@
+﻿//Your task is to correct the errors in the digitized text. You only have to handle the following mistakes:
+//A is misinterpreted as 4
+//S is misinterpreted as 5
+//O is misinterpreted as 0
+//I is misinterpreted as 1
+//The test cases contain numbers only by mistake.
+function keyboardMistakes(str) {
+    return str.replace(/4/g, "A").replace(/5/g, "S").replace(/0/g, "O").replace(/1/g, "I")
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(keyboardMistakes("MUB45H1R"), "MUBASHIR")
+assertEquals(keyboardMistakes("DUBL1N"), "DUBLIN")
+assertEquals(keyboardMistakes("51NG4P0RE"), "SINGAPORE")
+assertEquals(keyboardMistakes("P4K15T4N"), "PAKISTAN")
+assertEquals(keyboardMistakes("P4R15"), "PARIS")
