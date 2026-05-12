cef0b27e18e7b368ce506803cc0da8ab53a9718c
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-05-12 20:17:13 +0200

    Solved 255 Arithmetic Progression

---
+++ Edabit/255 Arithmetic Progression.js
@@ -0,0 +1,10 @@
+﻿//Return the first n elements of the sequence with the given common difference diff.
+//The final result should be a string of numbers, separated by comma and space.
+function arithmeticProgression(start, diff, n) {
+    return Array.from({ length: n }, (_, i) => start + diff * i).join(", ")
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(arithmeticProgression(1, 2, 5), "1, 3, 5, 7, 9")
+assertEquals(arithmeticProgression(1, 0, 5), "1, 1, 1, 1, 1")
+assertEquals(arithmeticProgression(1, -3, 10), "1, -2, -5, -8, -11, -14, -17, -20, -23, -26")
+assertEquals(arithmeticProgression(100, -10, 10), "100, 90, 80, 70, 60, 50, 40, 30, 20, 10")
