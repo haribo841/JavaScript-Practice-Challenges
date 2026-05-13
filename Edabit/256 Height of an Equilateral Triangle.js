0b70a8e2b2cde964bf2037ba263c47938c7f3057
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-05-13 13:15:31 +0200

    Solved 256 Height of an Equilateral Triangle

---
+++ Edabit/256 Height of an Equilateral Triangle.js
@@ -0,0 +1,12 @@
+﻿//Create a function that takes the length of the side of an equilateral triangle in centimeters and returns the height of the triangle in millimeters.
+function height(side) {
+    return (side * Math.sqrt(3) / 2 * 10).toFixed(1) + " mm"
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(height(2), "17.3 mm")
+assertEquals(height(5), "43.3 mm")
+assertEquals(height(6.2), "53.7 mm")
+assertEquals(height(8.7), "75.3 mm")
+assertEquals(height(10), "86.6 mm")
+assertEquals(height(13), "112.6 mm")
+assertEquals(height(18.5), "160.2 mm")
