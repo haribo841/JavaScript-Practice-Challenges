40345fd73f64eaf5001a46e6c1ed74e7eb41e2cb
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-05-18 22:56:26 +0200

    Solved 261 Return the Total Number of Parameters

---
+++ Edabit/261 Return the Total Number of Parameters.js
@@ -0,0 +1,9 @@
+﻿//Create a function that returns the total number of parameters passed in.
+function numberArgs(/* fill-in */) {
+    return arguments.length
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(numberArgs('a', 'b', 'c'), 3)
+assertEquals(numberArgs(10, 20, 30, 40, 50), 5)
+assertEquals(numberArgs('x', 'y'), 2)
+assertEquals(numberArgs(), 0)
