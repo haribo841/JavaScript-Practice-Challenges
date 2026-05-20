db8da8cd8fca72aaf25f968ea16dc309d8bb6fef
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-05-20 14:02:41 +0200

    Solved 263 Check if Number is within a Given Range

---
+++ Edabit/263 Check if Number is within a Given Range.js
@@ -0,0 +1,15 @@
+﻿//Given a number and an object with min and max properties, return true if the number lies within the given range (inclusive).
+function isInRange(num, range) {
+        return num >= range.min && num <= range.max;
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(isInRange(4, { min: 0, max: 5 }), true)
+assertEquals(isInRange(4, { min: 4, max: 5 }), true)
+assertEquals(isInRange(4, { min: 0, max: 4 }), true)
+assertEquals(isInRange(4, { min: 6, max: 10 }), false)
+assertEquals(isInRange(11, { min: 6, max: 10 }), false)
+assertEquals(isInRange(5, { min: 5, max: 5 }), true)
+assertEquals(isInRange(1.5, { min: 1.25, max: 1.75 }), true)
+assertEquals(isInRange(1.1, { min: 1.25, max: 1.75 }), false)
+assertEquals(isInRange(1.8, { min: 1.25, max: 1.75 }), false)
+assertEquals(isInRange(-1, { min: -1, max: 1 }), true)
