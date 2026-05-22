d113f72ac59495bf29bbdf346d21efb03c1fceeb
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-05-22 21:12:42 +0200

    Solved 265 Return Sole Element in a Set

---
+++ Edabit/265 Return Sole Element in a Set.js
@@ -0,0 +1,16 @@
+﻿//Given a set containing an element, return the sole element.
+function elementSet(s) {
+        return s.values().next().value;
+}
+import { assertEquals } from '../testHelper.js';
+const first = new Set();
+first.add(1);
+assertEquals(elementSet(first), 1)
+
+const second = new Set();
+second.add("apple");
+assertEquals(elementSet(second), "apple")
+
+const third = new Set();
+third.add(false);
+assertEquals(elementSet(third), false)
