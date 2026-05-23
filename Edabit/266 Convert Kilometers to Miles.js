53c7cf9a84b897354fce3910e5bb962480cfd7b0
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-05-23 11:37:27 +0200

    Solved 266 Convert Kilometers to Miles

---
+++ Edabit/266 Convert Kilometers to Miles.js
@@ -0,0 +1,9 @@
+﻿//In this challenge, you have to implement a function that returns the given distance kilometers converted into miles.
+//You have to round the result up to the fifth decimal digit.
+function kmtomiles(kilometers) {
+    return Math.round(kilometers * 0.621371 * 100000) / 100000
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(kmtomiles(2), 1.24274)
+assertEquals(kmtomiles(6), 3.72823)
+assertEquals(kmtomiles(8), 4.97097)
