1ae3773baf84a18c57be4c1856137e9c4aa6738c
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-05-26 08:40:43 +0200

    Solved 269 Nothing is Nothing

---
+++ Edabit/269 Nothing is Nothing.js
@@ -0,0 +1,12 @@
+﻿//Given any number of parameters, return true if none of the arguments are falsy.
+function nothingIsNothing() {
+    return [...arguments].every(arg => Boolean(arg));
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(nothingIsNothing(0, false, [], {}), false)
+assertEquals(nothingIsNothing(33, 'Hello', (true, true, 3)), true)
+assertEquals(nothingIsNothing(true, null), false)
+assertEquals(nothingIsNothing(null, null), false)
+assertEquals(nothingIsNothing(221), true)
+assertEquals(nothingIsNothing(221, 0, 0, 0), false)
+assertEquals(nothingIsNothing([221, 0, 0, 0]), true)
