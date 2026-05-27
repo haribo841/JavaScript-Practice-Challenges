64825919b397ddff54c190ecf490ac10f453e01a
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-05-27 11:42:13 +0200

    Solved 270 Rotate for Max Number

---
+++ Edabit/270 Rotate for Max Number.js
@@ -0,0 +1,11 @@
+﻿//Create a function which takes a number and returns the maximum value by rearranging its digits.
+function rotateMaxNumber(num) {
+    return parseInt(String(num).split('').sort((a, b) => b - a).join(''), 10);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(rotateMaxNumber(123), 321)
+assertEquals(rotateMaxNumber(1546), 6541)
+assertEquals(rotateMaxNumber("001"), 100)
+assertEquals(rotateMaxNumber(999), 999)
+assertEquals(rotateMaxNumber("12345"), 54321)
+assertEquals(rotateMaxNumber("00009"), 90000)
