1c0d15e8056ae84b417c7cf83573434057edbe5e
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-05-29 18:15:19 +0200

    Solved 272 Less Than 100 Array Remix

---
+++ Edabit/272 Less Than 100 Array Remix.js
@@ -0,0 +1,11 @@
+﻿//Given an array of numbers, return true if the sum of the array is less than 100; otherwise return false.
+function arrayLessThan100(arr) {
+    return arr.reduce((acc, num) => acc + num, 0) < 100;
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(arrayLessThan100([5, 57]), true)
+assertEquals(arrayLessThan100([77, 30]), false)
+assertEquals(arrayLessThan100([0, 59, 15]), true)
+assertEquals(arrayLessThan100([0]), true)
+assertEquals(arrayLessThan100([35, 59, 15]), false)
+assertEquals(arrayLessThan100([25, 50, 25]), false)
