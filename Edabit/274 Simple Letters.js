2090a8a865cef261bc4a4d1ff1a0ebe2c668beff
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-05-31 11:53:43 +0200

    Solved 274 Simple Letters

---
+++ Edabit/274 Simple Letters.js
@@ -0,0 +1,14 @@
+﻿//Create a function that takes two lowercase strings str1 and str2 of letters from a to z and returns the sorted and longest string containing distinct letters.
+function longestString(str1, str2) {
+    return [...new Set(str1 + str2)].sort().join('');
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(longestString("mubashir", "edabit"), "abdehimrstu")
+assertEquals(longestString("pakistan", "airforce"), "acefiknoprst")
+assertEquals(longestString("", "e"), "e")
+assertEquals(longestString("", ""), "")
+assertEquals(longestString("noooo", "yesssss"), "enosy")
+assertEquals(longestString("aretheyhere", "yestheyarehere"), "aehrsty")
+assertEquals(longestString("loopingisfunbutdangerous", "lessdangerousthancoding"), "abcdefghilnoprstu")
+assertEquals(longestString("inmanylanguages", "theresapairoffunctions"), "acefghilmnoprstuy")
+assertEquals(longestString("lordsofthefallen", "gamekult"), "adefghklmnorstu")
