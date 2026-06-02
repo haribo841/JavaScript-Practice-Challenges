65ab593f021184d0b2992d81369c1cfad8962430
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-06-02 23:26:31 +0200

    Solved 276 Radian to Degree

---
+++ Edabit/276 Radian to Degree.js
@@ -0,0 +1,10 @@
+﻿//Create a function that takes an angle in radians and converts it into degrees.
+function toDegree(radian) {
+    return Math.round(radian * (180 / Math.PI) * 100) / 100;
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(toDegree(2 * Math.PI), 360)
+assertEquals(toDegree(Math.PI), 180)
+assertEquals(toDegree(Math.PI / 2), 90)
+assertEquals(toDegree(Math.PI / 4), 45)
+assertEquals(toDegree(Math.PI / 3), 60)
