c59797a8d85dfd1672c9207b1f3cdeb53691d981
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-06-04 11:54:12 +0200

    Solved 278 Object Contain a Given Key

---
+++ Edabit/278 Object Contain a Given Key.js
@@ -0,0 +1,9 @@
+﻿//Write a function that returns true if a hash contains the specified key, and false otherwise.
+function hasKey(obj, key) {
+    return obj.hasOwnProperty(key);
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(hasKey({ pot: 1, tot: 2, not: 3 }, "not"), true)
+assertEquals(hasKey({ craves: true, midnight: true, snack: true }, "morning"), false)
+assertEquals(hasKey({ a: 44, b: 45, c: 46 }, "d"), false)
+assertEquals(hasKey({ a: "z", b: "y", c: "x" }, "c"), true)
