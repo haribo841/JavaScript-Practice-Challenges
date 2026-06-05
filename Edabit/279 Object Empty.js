051afbe726575591b3fd13b3caf9483ba63404e2
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-06-05 13:09:57 +0200

    Solved 279 Object Empty

---
+++ Edabit/279 Object Empty.js
@@ -0,0 +1,8 @@
+﻿//Write a function that returns true if an object is empty, and false otherwise.
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(isEmpty({}), true)
+assertEquals(isEmpty({ a: 1 }), false)
+assertEquals(isEmpty({ z: 2, w: 4, y: 5 }), false)
