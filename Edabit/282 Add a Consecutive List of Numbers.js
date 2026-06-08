e2b93a1c0d7091f91f57a0d80ac05270a116707a
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-06-08 07:56:22 +0200

    Solved 282 Add a Consecutive List of Numbers

---
+++ Edabit/282 Add a Consecutive List of Numbers.js
@@ -0,0 +1,11 @@
+﻿//Write a function that takes the last number of a consecutive list of numbers and returns the total of all numbers up to and including it.
+function addUpTo(n) {
+    return n * (n + 1) / 2;
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(addUpTo(3), 6, "Example #1")
+assertEquals(addUpTo(10), 55, "Example #2")
+assertEquals(addUpTo(0), 0)
+assertEquals(addUpTo(2), 3)
+assertEquals(addUpTo(20), 210)
+assertEquals(addUpTo(1), 1)
