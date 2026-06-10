280926a3653bec1ceb39fce0e8d94105ff84ad33
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-06-10 11:07:58 +0200

    Solved 284 Add, Subtract, Multiply or Divide

---
+++ Edabit/284 Add, Subtract, Multiply or Divide.js
@@ -0,0 +1,14 @@
+﻿//Write a function that takes two numbers and returns if they should be added, subtracted, multiplied or divided to get 24.
+//If none of the operations can give 24, return null.
+function operation(num1, num2) {
+    return num1 + num2 === 24 ? "added" :
+        num1 - num2 === 24 ? "subtracted" :
+            num1 * num2 === 24 ? "multiplied" :
+                num1 / num2 === 24 ? "divided" : null
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(operation(12, 12), "added")
+assertEquals(operation(100, 76), "subtracted")
+assertEquals(operation(6, 4), "multiplied")
+assertEquals(operation(528, 22), "divided")
+assertEquals(operation(10, 12), null)
