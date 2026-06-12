320c88e32bf0cf0375dafd645adf49a6543b61ba
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-06-12 21:33:52 +0200

    Solved 286 Total Number of Digits the Given Number Has

---
+++ Edabit/286 Total Number of Digits the Given Number Has.js
@@ -0,0 +1,11 @@
+﻿//Create a function that takes a number as an argument and returns the amount of digits it has.
+function findDigitAmount(num) {
+    return String(num).length;
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(findDigitAmount(1), 1, 'Should work with one digit')
+assertEquals(findDigitAmount(67), 2, 'Should work with a two-digit number')
+assertEquals(findDigitAmount(123), 3, 'Should work with a three-digit number')
+assertEquals(findDigitAmount(55551), 5, 'Should work with a bit bigger number')
+assertEquals(findDigitAmount(96456431), 8, 'Should work with bigger numbers')
+assertEquals(findDigitAmount(0), 1, '0 should return 1, as it is a digit')
