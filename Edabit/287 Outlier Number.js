9ae998e663a41523a4478c9ea1fcda3360b496ed
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-06-14 12:12:19 +0200

    Solved 287 Outlier Number

---
+++ Edabit/287 Outlier Number.js
@@ -0,0 +1,19 @@
+﻿//Given an array of either entirely odd integers or entirely even integers except for a single Outlier Number, create a function to return this number.
+function outlierNumber(arr) {
+    return arr.filter(x => x % 2 === 0).length === 1 ? arr.filter(x => x % 2 === 0)[0] : arr.filter(x => x % 2 !== 0)[0]
+}
+import { assertEquals } from '../testHelper.js';
+assertEquals(outlierNumber([2, 3, 4]), 3)
+assertEquals(outlierNumber([1, 2, 3]), 2)
+assertEquals(outlierNumber([4, 1, 3, 5, 9]), 4)
+assertEquals(outlierNumber([2, 6, 8, 10, 3]), 3)
+assertEquals(outlierNumber([2, 6, 8, 200, 700, 1, 84, 10, 4]), 1)
+assertEquals(outlierNumber([17, 6, 8, 10, 6, 12, 24, 36]), 17)
+assertEquals(outlierNumber([1, 1, 1, 1, 1, 44, 7, 7, 7, 7, 7, 7, 7, 7]), 44)
+assertEquals(outlierNumber([3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 35, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7, 7, 7, 7, 1000]), 1000)
+assertEquals(outlierNumber([1, 0, 0]), 1)
+assertEquals(outlierNumber([3, 7, -99, 81, 90211, 0, 7]), 0)
+assertEquals(outlierNumber([2, -6, 8, -10, -3]), -3)
+assertEquals(outlierNumber([2, 6, 8, 2, -66, 34, -35, 66, 700, 1002, -84, 10, 4]), -35)
+assertEquals(outlierNumber([-20, 1, 7, 17, 19, 211, 7]), -20)
+assertEquals(outlierNumber([1, 1, -1, 1, 1, -44, 7, 7, 7, 7, 7, 7, 7, 7]), -44)
