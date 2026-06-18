f4d1d0ce59e2331037a4b5091569b39e6a07131b
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-06-18 19:08:46 +0200

    Solved 291 Say What

---
+++ Edabit/291 Say What.js
@@ -0,0 +1,11 @@
+﻿//Given an object, return a string that concatenates all the values and adds the 2nd key at the end.
+//Make sure you keep an empty space between them but not at the beginning or end of the string.
+function sayWhat(obj) {
+    return `${obj[1]} ${obj[2]} ${obj[3]} ${obj[2]}`;
+}
+import { assertEquals } from '../testHelper.js';
+const random = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+const obj = { 1: "Mommy", 2: "please", 3: "help" }
+const obj2 = { 1: random, 2: "innocent", 3: "is" }
+assertEquals(sayWhat(obj), "Mommy please help please")
+assertEquals(sayWhat(obj2), `${obj2[1]} innocent is innocent`)
