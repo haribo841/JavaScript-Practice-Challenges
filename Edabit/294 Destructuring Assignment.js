b9fd585f9f15908cad47b797b1cc41b02e6317a6
Adam Kubiś <48011447+haribo841@users.noreply.github.com>
2026-06-23 21:27:52 +0200

    Solved 294 Destructuring Assignment

---
+++ Edabit/294 Destructuring Assignment.js
@@ -0,0 +1,5 @@
+﻿//Using Destructuring Assignment, your task is to unpack the arrays writeyourcodehere into three variables, first, a variable to ignore all middle values and last.
+const [first, , , , , , , last] = [1, 2, 3, 4, 5, 6, 7, 8];
+import { assertEquals } from '../testHelper.js';
+assertEquals(first, 1)
+assertEquals(last, 8)
