7b48327217ba2f8c4a7040d8c42288d8031afbbb
Adam Kubiś <adamwaldemarkubis@gmail.com>
2026-08-27 21:24:43 +0200

    Fix 304 Minimal to Reject Callback

---
+++ Edabit/304 Reject Callback.js
@@ -0,0 +1,25 @@
+﻿//Reject the promise with the simple string "It's not a dog!".
+import { setTimeout } from "node:timers";
+const promise = new Promise((resolve, reject) => {
+    const animal = "cat";
+
+    setTimeout(() => {
+        if (animal === "dog") {
+            resolve("It's a dog!");
+        } else {
+            reject("It's not a dog!");
+        }
+    }, 1000);
+});
+import { assertEquals } from "../testHelper.js";
+
+assertEquals(promise instanceof Promise, true);
+
+const rejection = await promise.then(
+    () => {
+        throw new Error("Expected the promise to reject.");
+    },
+    reason => reason
+);
+
+assertEquals(rejection, "It's not a dog!");
